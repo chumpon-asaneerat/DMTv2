6afd41c7cce4af66ded5e708076b0911511ea1d8
chumpon-asaneerat <chumponsenate@gmail.com>
2020-06-23 08:52:21 +0700

    Update 2020-06-23 #2 - Restructure Controls folder #2. - Add RouteConsts.cs file. - Cleanup bin folders. - Re compile all projects.

---
+++ 01Core/01.DMT.Core/RouteConsts.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DMT
+{
+    public static class RouteConsts
+    {
+        public const string Url = @"api";
+        public static class Job
+        {
+            public const string Url = RouteConsts.Url + @"/Job";
+
+            public static class BeginJob
+            {
+                public const string Url = Job.Url + @"/BeginJob";
+                public const string Name = "BeginJob";
+            }
+            public static class EndJob
+            {
+                public const string Url = Job.Url + @"/EndJob";
+                public const string Name = "EndJob";
+            }
+        }
+    }
+}
