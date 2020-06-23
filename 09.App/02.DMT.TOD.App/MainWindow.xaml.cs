397f53fb855b29dd9e39d092c2c83b4e42e1b480
chumpon-asaneerat <chumponsenate@gmail.com>
2020-06-23 12:15:22 +0700

    Update 2020-06-23 #8 - Portd TOD app project from v1. - Portd TA app project from v1. - Portd Config app project from v1. - Recompile all projects.

---
+++ 09.App/02.DMT.TOD.App/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿#region Using
+
+using System;
+using System.Windows;
+
+using NLib.Services;
+
+//using DMT.Models;
+//using DMT.Services;
+
+#endregion
+
+namespace DMT
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Load/Unload
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Start app manager.
+            //DMTPlazaManager.Instance.Start();
+            // Initial Page Content Manager
+            PageContentManager.Instance.ContentChanged += new EventHandler(Instance_ContentChanged);
+            PageContentManager.Instance.StatusUpdated += new StatusMessageEventHandler(Instance_StatusUpdated);
+            PageContentManager.Instance.OnTick += new EventHandler(Instance_OnTick);
+            PageContentManager.Instance.Start();
+            // Init Main Menu
+            //PageContentManager.Instance.Current = new TOD.Pages.MainMenu();
+        }
+
+        private void Window_Unloaded(object sender, RoutedEventArgs e)
+        {
+            // Release Page Content Manager
+            PageContentManager.Instance.Shutdown();
+            PageContentManager.Instance.OnTick -= new EventHandler(Instance_OnTick);
+            PageContentManager.Instance.StatusUpdated -= new StatusMessageEventHandler(Instance_StatusUpdated);
+            PageContentManager.Instance.ContentChanged -= new EventHandler(Instance_ContentChanged);
+            // Shutdown app manager.
+            //DMTPlazaManager.Instance.Shutdown();
+        }
+
+        #endregion
+
+        #region Page Content Manager Handlers
+
+        void Instance_OnTick(object sender, EventArgs e)
+        {
+            //UpdateTime();
+            //UpdateConnectionStatus();
+        }
+
+        void Instance_StatusUpdated(object sender, StatusMessageEventArgs e)
+        {
+            //txtStatus.Text = e.Message;
+        }
+
+        void Instance_ContentChanged(object sender, EventArgs e)
+        {
+            this.container.Content = PageContentManager.Instance.Current;
+        }
+
+        #endregion
+    }
+}
---
+++ 09.App/04.DMT.Plaza.Config.App/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿#region Using
+
+using System;
+using System.Security.Principal;
+using System.Windows;
+
+using NLib.Services;
+
+//using DMT.Models;
+using DMT.Services;
+
+using Fluent;
+
+#endregion
+
+namespace DMT
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : RibbonWindow
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Loaded/Unloaded
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DMTServiceOperations.Instance.ServiceMonitor.ScanConpleted += ServiceMonitor_ScanConpleted;
+            DMTServiceOperations.Instance.ServiceMonitor.Start();
+        }
+
+        private void Window_Unloaded(object sender, RoutedEventArgs e)
+        {
+            DMTServiceOperations.Instance.ServiceMonitor.Shutdown();
+            DMTServiceOperations.Instance.ServiceMonitor.ScanConpleted -= ServiceMonitor_ScanConpleted;
+        }
+
+        #endregion
+
+        class InstallStatus
+        {
+
+        }
+
+        private void ServiceMonitor_ScanConpleted(object sender, EventArgs e)
+        {
+
+        }
+
+        #region Button Handlers
+
+        #region Windows Services (Install/Unstall/CheckStatus)
+
+        private void cmdInstall_Click(object sender, RoutedEventArgs e)
+        {
+            DMTServiceOperations.Instance.Install();
+        }
+
+        private void cmdUninstall_Click(object sender, RoutedEventArgs e)
+        {
+            DMTServiceOperations.Instance.Uninstall();
+        }
+
+        private void cmdCheckWindowServiceStatus_Click(object sender, RoutedEventArgs e)
+        {
+            var status = DMTServiceOperations.Instance.CheckInstalled();
+            if (status.PlazaLocalServiceInstalled)
+            {
+                MessageBox.Show("Plaza Sercice installed and running");
+            }
+            else
+            {
+                MessageBox.Show("Plaza Sercice is not installed or stopped");
+            }
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
