e48f5310c19a42b56408707d6a32ec4e18c8ef03
chumpon-asaneerat <chumponsenate@gmail.com>
2020-06-23 12:49:00 +0700

    Update 2020-06-23 #10 - DMT.Controls project - Add MenuButton Control. - DMT.Controls project - Add ImageButton Control.

---
+++ 04.Controls/01.DMT.Controls/Common/ImageButton.xaml.cs
@@ -0,0 +1,54 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+#endregion
+
+namespace DMT.Controls
+{
+    /// <summary>
+    /// Interaction logic for ImageButton.xaml
+    /// </summary>
+    public partial class ImageButton : UserControl
+    {
+        public ImageButton()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty ImageSourceProperty =
+            DependencyProperty.Register("ImageSource", typeof(ImageSource), typeof(ImageButton), new PropertyMetadata(null));
+
+        public ImageSource ImageSource
+        {
+            get
+            {
+                return (ImageSource)GetValue(ImageSourceProperty);
+            }
+            set
+            {
+                SetValue(ImageSourceProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty TextContentProperty = DependencyProperty.Register("TextContent", typeof(object), typeof(ImageButton), new PropertyMetadata(null));
+        public object TextContent
+        {
+            get { return (object)GetValue(TextContentProperty); }
+            set { SetValue(TextContentProperty, value); }
+        }
+    }
+}
---
+++ 04.Controls/01.DMT.Controls/Common/MenuButton.xaml.cs
@@ -0,0 +1,47 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+#endregion
+
+namespace DMT.Controls
+{
+    /// <summary>
+    /// Interaction logic for MenuButton.xaml
+    /// </summary>
+    public partial class MenuButton : UserControl
+    {
+        public MenuButton()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty ImageSourceProperty =
+            DependencyProperty.Register("ImageSource", typeof(ImageSource), typeof(ImageButton), new PropertyMetadata(null));
+
+        public ImageSource ImageSource
+        {
+            get
+            {
+                return (ImageSource)GetValue(ImageSourceProperty);
+            }
+            set
+            {
+                SetValue(ImageSourceProperty, value);
+            }
+        }
+    }
+}
