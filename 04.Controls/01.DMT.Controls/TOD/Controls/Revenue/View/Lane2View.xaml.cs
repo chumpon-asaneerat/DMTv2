527aa3aabd56feec1b563fb832d64958cb4f05ad
vorakanok <vorakanok@yahoo.com>
2020-06-24 14:52:48 +0700

    Update form

---
+++ 04.Controls/01.DMT.Controls/TOD/Controls/Revenue/View/Lane2View.xaml.cs
@@ -0,0 +1,50 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+#endregion
+
+namespace DMT.TOD.Controls.Revenue.View
+{
+    /// <summary>
+    /// Interaction logic for Lane2View.xaml
+    /// </summary>
+    public partial class Lane2View : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public Lane2View()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Loaded/Unloaded
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        /*
+        public void Setup(List<Models.Lane> lanes)
+        {
+            listView.ItemsSource = lanes;
+        }
+        */
+    }
+}
