ee822779a836e58e52a8e5355ec65498368a8f97
vorakanok <vorakanok@yahoo.com>
2020-06-25 23:22:58 +0700

    Update Form TOD

---
+++ 04.Controls/01.DMT.Controls/Header/Elements/HeaderPlazaTOD.xaml.cs
@@ -0,0 +1,45 @@
+﻿#region Using
+
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+//using NLib.Services;
+//using DMT.Services;
+
+#endregion
+
+namespace DMT.Controls.Header
+{
+    /// <summary>
+    /// Interaction logic for HeaderPlazaTOD.xaml
+    /// </summary>
+    public partial class HeaderPlazaTOD : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public HeaderPlazaTOD()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Loaded/Unloaded
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+    }
+}
