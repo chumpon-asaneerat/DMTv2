07b5cd2b0e998823eb82e122c62cafd7a29beea5
chumpon-asaneerat <chumponsenate@gmail.com>
2020-06-25 22:53:50 +0700

    Update 2020-06-25 #8 - Add NRestClient.cs file. - Implements NRestClient class.

--- 02.Models/01.DMT.Models/Models/NTables.cs
+++ 02.Models/01.DMT.Models/Models/NTables.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#region Using
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -11,6 +13,8 @@ using DMT.Services;
 using Newtonsoft.Json;
 using NLib;
 using NLib.Reflection;
+
+#endregion
 
 namespace DMT.Models
 {
