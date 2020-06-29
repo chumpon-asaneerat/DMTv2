0d851377d5f146c424a7edb15a7a2e1f60fe9ff4
chumpon-asaneerat <chumponsenate@gmail.com>
2020-06-29 21:12:07 +0700

    Update 2020-06-29 #23 - Reimplements models and local db server #21.

--- 09.App/05.DMT.Plaza.Simulator.App/MainWindow.xaml.cs
+++ 09.App/05.DMT.Plaza.Simulator.App/MainWindow.xaml.cs
@@ -7,7 +7,7 @@ using System.Windows;
 using NLib.Services;
 
 //using DMT.Models;
-//using DMT.Services;
+using DMT.Services;
 
 using Fluent;
 
@@ -36,13 +36,56 @@ namespace DMT
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            DMTServiceOperations.Instance.ServiceMonitor.ScanConpleted += ServiceMonitor_ScanConpleted;
+            DMTServiceOperations.Instance.ServiceMonitor.Start();
 
+            // Initial Page Content Manager
+            PageContentManager.Instance.ContentChanged += new EventHandler(Instance_ContentChanged);
+            PageContentManager.Instance.StatusUpdated += new StatusMessageEventHandler(Instance_StatusUpdated);
+            PageContentManager.Instance.OnTick += new EventHandler(Instance_OnTick);
+            PageContentManager.Instance.Start();
         }
 
         private void Window_Unloaded(object sender, RoutedEventArgs e)
         {
+            // Release Page Content Manager
+            PageContentManager.Instance.Shutdown();
+            PageContentManager.Instance.OnTick -= new EventHandler(Instance_OnTick);
+            PageContentManager.Instance.StatusUpdated -= new StatusMessageEventHandler(Instance_StatusUpdated);
+            PageContentManager.Instance.ContentChanged -= new EventHandler(Instance_ContentChanged);
+
+            DMTServiceOperations.Instance.ServiceMonitor.Shutdown();
+            DMTServiceOperations.Instance.ServiceMonitor.ScanConpleted -= ServiceMonitor_ScanConpleted;
+        }
+
+        #endregion
+
+        #region Page Content Manager Handlers
+
+        void Instance_OnTick(object sender, EventArgs e)
+        {
+            //UpdateTime();
+            //UpdateConnectionStatus();
+        }
+
+        void Instance_StatusUpdated(object sender, StatusMessageEventArgs e)
+        {
+            //txtStatus.Text = e.Message;
+        }
+
+        void Instance_ContentChanged(object sender, EventArgs e)
+        {
+            this.container.Content = PageContentManager.Instance.Current;
+        }
+
+        #endregion
+
+        private void ServiceMonitor_ScanConpleted(object sender, EventArgs e)
+        {
 
         }
+
+        #region Button Handlers
 
         #endregion
 
@@ -53,12 +96,14 @@ namespace DMT
             //Console.WriteLine(result);
             MessageBox.Show(result);
             */
+            /*
             var user = Models.User.Create();
             user.UserId = "99001";
             var ret = Services.DMTServiceOperations.Instance.Plaza.GetUser(user);
             if (null != ret)
                 MessageBox.Show("Found :" + ret.UserName);
             else MessageBox.Show("User not Found");
+            */
         }
     }
 }
