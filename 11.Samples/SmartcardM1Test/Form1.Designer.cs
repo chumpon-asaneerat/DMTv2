e441a4b8d9b005bd2441ca5faad6318bf8b2d97c
chumpon-asaneerat <chumponsenate@gmail.com>
2020-07-07 02:03:37 +0700

    Update 2020-07-07 #2 - Update smartcard lib performance tuning #2.

--- 11.Samples/SmartcardM1Test/Form1.Designer.cs
+++ 11.Samples/SmartcardM1Test/Form1.Designer.cs
@@ -45,43 +45,47 @@
             // lbBlock3
             // 
             this.lbBlock3.AutoSize = true;
-            this.lbBlock3.Location = new System.Drawing.Point(123, 240);
+            this.lbBlock3.Font = new System.Drawing.Font("Lucida Console", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbBlock3.Location = new System.Drawing.Point(113, 243);
             this.lbBlock3.Name = "lbBlock3";
-            this.lbBlock3.Size = new System.Drawing.Size(62, 17);
+            this.lbBlock3.Size = new System.Drawing.Size(79, 14);
             this.lbBlock3.TabIndex = 15;
             this.lbBlock3.Text = "Block 0 :";
             // 
             // lbBlock2
             // 
             this.lbBlock2.AutoSize = true;
-            this.lbBlock2.Location = new System.Drawing.Point(123, 210);
+            this.lbBlock2.Font = new System.Drawing.Font("Lucida Console", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbBlock2.Location = new System.Drawing.Point(113, 213);
             this.lbBlock2.Name = "lbBlock2";
-            this.lbBlock2.Size = new System.Drawing.Size(62, 17);
+            this.lbBlock2.Size = new System.Drawing.Size(79, 14);
             this.lbBlock2.TabIndex = 14;
             this.lbBlock2.Text = "Block 0 :";
             // 
             // lbBlock1
             // 
             this.lbBlock1.AutoSize = true;
-            this.lbBlock1.Location = new System.Drawing.Point(123, 183);
+            this.lbBlock1.Font = new System.Drawing.Font("Lucida Console", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbBlock1.Location = new System.Drawing.Point(113, 186);
             this.lbBlock1.Name = "lbBlock1";
-            this.lbBlock1.Size = new System.Drawing.Size(62, 17);
+            this.lbBlock1.Size = new System.Drawing.Size(79, 14);
             this.lbBlock1.TabIndex = 13;
             this.lbBlock1.Text = "Block 0 :";
             // 
             // lbBlock0
             // 
             this.lbBlock0.AutoSize = true;
-            this.lbBlock0.Location = new System.Drawing.Point(123, 156);
+            this.lbBlock0.Font = new System.Drawing.Font("Lucida Console", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbBlock0.Location = new System.Drawing.Point(113, 159);
             this.lbBlock0.Name = "lbBlock0";
-            this.lbBlock0.Size = new System.Drawing.Size(62, 17);
+            this.lbBlock0.Size = new System.Drawing.Size(79, 14);
             this.lbBlock0.TabIndex = 12;
             this.lbBlock0.Text = "Block 0 :";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(34, 156);
+            this.label4.Location = new System.Drawing.Point(23, 156);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(84, 17);
             this.label4.TabIndex = 11;
@@ -90,7 +94,7 @@
             // lbCardExist
             // 
             this.lbCardExist.AutoSize = true;
-            this.lbCardExist.Location = new System.Drawing.Point(123, 100);
+            this.lbCardExist.Location = new System.Drawing.Point(113, 100);
             this.lbCardExist.Name = "lbCardExist";
             this.lbCardExist.Size = new System.Drawing.Size(62, 17);
             this.lbCardExist.TabIndex = 10;
@@ -99,7 +103,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(34, 100);
+            this.label1.Location = new System.Drawing.Point(23, 100);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(83, 17);
             this.label1.TabIndex = 9;
@@ -110,7 +114,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Red;
-            this.label2.Location = new System.Drawing.Point(100, 9);
+            this.label2.Location = new System.Drawing.Point(137, 9);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(378, 17);
             this.label2.TabIndex = 16;
@@ -121,7 +125,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Red;
-            this.label3.Location = new System.Drawing.Point(100, 39);
+            this.label3.Location = new System.Drawing.Point(137, 39);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(367, 17);
             this.label3.TabIndex = 17;
@@ -130,16 +134,17 @@
             // lbSN
             // 
             this.lbSN.AutoSize = true;
-            this.lbSN.Location = new System.Drawing.Point(123, 127);
+            this.lbSN.Font = new System.Drawing.Font("Lucida Console", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSN.Location = new System.Drawing.Point(113, 130);
             this.lbSN.Name = "lbSN";
-            this.lbSN.Size = new System.Drawing.Size(13, 17);
+            this.lbSN.Size = new System.Drawing.Size(15, 14);
             this.lbSN.TabIndex = 19;
             this.lbSN.Text = "-";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(44, 127);
+            this.label5.Location = new System.Drawing.Point(33, 127);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(73, 17);
             this.label5.TabIndex = 18;
@@ -162,7 +167,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(586, 294);
+            this.ClientSize = new System.Drawing.Size(708, 314);
             this.Controls.Add(this.chkReadBoth);
             this.Controls.Add(this.lbSN);
             this.Controls.Add(this.label5);
