ab7c935169559cf8d014bd8fa4cd6001c526eed6
vorakanok <vorakanok@yahoo.com>
2020-07-12 23:06:37 +0700

    Update TA

---
+++ 05.Controls/01.DMT.Controls/TA/Windows/Collector/Credit/MessageWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace DMT.TA.Windows.Collector.Credit
+{
+    /// <summary>
+    /// Interaction logic for MessageWindow.xaml
+    /// </summary>
+    public partial class MessageWindow : Window
+    {
+        public MessageWindow()
+        {
+            InitializeComponent();
+            this.Topmost = true;
+        }
+
+        private void cmdOK_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+        private void cmdCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+
+        public void Setup(string des, string total)
+        {
+
+            txtDescription.Text = des + " จำนวนเงิน " + total + " บาท";
+
+        }
+
+
+    }
+}
