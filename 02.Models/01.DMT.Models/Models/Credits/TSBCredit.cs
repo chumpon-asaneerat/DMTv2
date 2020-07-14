067a10ad049973a40dedb4b219bd46b06235cdb6
chumpon-asaneerat <chumponsenate@gmail.com>
2020-07-15 00:06:27 +0700

    Update 2020-07-15 #1 - Update TA #2   - Implement TSBCredit Model #1.   - Implement UserCredit Model #1.

---
+++ 02.Models/01.DMT.Models/Models/Credits/TSBCredit.cs
@@ -0,0 +1,241 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+using SQLiteNetExtensions.Extensions;
+
+// required for JsonIgnore.
+using Newtonsoft.Json;
+using NLib;
+using NLib.Reflection;
+
+#endregion
+
+namespace DMT.Models
+{
+    #region Enum
+
+    public enum TSBCreditTransactionType : int
+    {
+        Init = 1
+    }
+
+    #endregion
+
+    #region TSBCredit
+
+    /// <summary>
+    /// The TSBCredit Data Model class.
+    /// </summary>
+    //[Table("TSBCredit")]
+    public class TSBCredit : NTable<TSBCredit>
+    {
+        #region Internal Variables
+
+        private TSBCreditTransactionType _transType = TSBCreditTransactionType.Init;
+
+        private string _TSBId = string.Empty;
+        private string _TSBNameEN = string.Empty;
+        private string _TSBNameTH = string.Empty;
+
+        private int _Status = 0;
+        private DateTime _LastUpdate = DateTime.MinValue;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public TSBCredit() : base() { }
+
+        #endregion
+
+        #region Public Properties
+
+        #region Common
+
+        /// <summary>
+        /// Gets or sets TransactionType.
+        /// </summary>
+        [PeropertyMapName("TransactionType")]
+        public TSBCreditTransactionType TransactionType
+        {
+            get
+            {
+                return _transType;
+            }
+            set
+            {
+                if (_transType != value)
+                {
+                    _transType = value;
+                    this.RaiseChanged("TransactionType");
+                }
+            }
+        }
+
+        #endregion
+
+        #region TSB
+
+        /// <summary>
+        /// Gets or sets TSBId.
+        /// </summary>
+        [MaxLength(10)]
+        [PeropertyMapName("TSBId")]
+        public string TSBId
+        {
+            get
+            {
+                return _TSBId;
+            }
+            set
+            {
+                if (_TSBId != value)
+                {
+                    _TSBId = value;
+                    this.RaiseChanged("TSBId");
+                }
+            }
+        }
+        /// <summary>
+        /// Gets or sets TSBNameEN.
+        /// </summary>
+        [Ignore]
+        [PeropertyMapName("TSBNameEN")]
+        public virtual string TSBNameEN
+        {
+            get
+            {
+                return _TSBNameEN;
+            }
+            set
+            {
+                if (_TSBNameEN != value)
+                {
+                    _TSBNameEN = value;
+                    this.RaiseChanged("TSBNameEN");
+                }
+            }
+        }
+        /// <summary>
+        /// Gets or sets TSBNameTH.
+        /// </summary>
+        [Ignore]
+        [PeropertyMapName("TSBNameTH")]
+        public virtual string TSBNameTH
+        {
+            get
+            {
+                return _TSBNameTH;
+            }
+            set
+            {
+                if (_TSBNameTH != value)
+                {
+                    _TSBNameTH = value;
+                    this.RaiseChanged("TSBNameTH");
+                }
+            }
+        }
+
+        #endregion
+
+        #region Status (DC)
+
+        /// <summary>
+        /// Gets or sets Status (1 = Sync, 0 = Unsync, etc..)
+        /// </summary>
+        [PeropertyMapName("Status")]
+        public int Status
+        {
+            get
+            {
+                return _Status;
+            }
+            set
+            {
+                if (_Status != value)
+                {
+                    _Status = value;
+                    this.RaiseChanged("Status");
+                }
+            }
+        }
+        /// <summary>
+        /// Gets or sets LastUpdated (Sync to DC).
+        /// </summary>
+        [PeropertyMapName("LastUpdate")]
+        public DateTime LastUpdate
+        {
+            get { return _LastUpdate; }
+            set
+            {
+                if (_LastUpdate != value)
+                {
+                    _LastUpdate = value;
+                    this.RaiseChanged("LastUpdate");
+                }
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Internal Class
+
+        public class FKs : TSBCredit
+        {
+            #region TSB
+
+            /// <summary>
+            /// Gets or sets TSBNameEN.
+            /// </summary>
+            [MaxLength(100)]
+            [PeropertyMapName("TSBNameEN")]
+            public override string TSBNameEN
+            {
+                get { return base.TSBNameEN; }
+                set { base.TSBNameEN = value; }
+            }
+            /// <summary>
+            /// Gets or sets TSBNameTH.
+            /// </summary>
+            [MaxLength(100)]
+            [PeropertyMapName("TSBNameTH")]
+            public override string TSBNameTH
+            {
+                get { return base.TSBNameTH; }
+                set { base.TSBNameTH = value; }
+            }
+
+            #endregion
+
+            #region Public Methods
+
+            public TSBCredit ToTSBCredit()
+            {
+                TSBCredit inst = new TSBCredit();
+                this.AssignTo(inst); // set all properties to new instance.
+                return inst;
+            }
+
+            #endregion
+        }
+
+        #endregion
+
+        #region Static Methods
+
+        #endregion
+    }
+
+    #endregion
+}
---
+++ 02.Models/01.DMT.Models/Models/Credits/UserCredit.cs
@@ -0,0 +1,304 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+using SQLiteNetExtensions.Extensions;
+
+// required for JsonIgnore.
+using Newtonsoft.Json;
+using NLib;
+using NLib.Reflection;
+
+#endregion
+
+namespace DMT.Models
+{
+    #region UserCredit
+
+    /// <summary>
+    /// The UserCredit Data Model class.
+    /// </summary>
+    //[Table("UserCredit")]
+    public class UserCredit : NTable<UserCredit>
+    {
+        #region Internal Variables
+
+        private string _TSBId = string.Empty;
+        private string _TSBNameEN = string.Empty;
+        private string _TSBNameTH = string.Empty;
+
+        private string _UserId = string.Empty;
+        private string _FullNameEN = string.Empty;
+        private string _FullNameTH = string.Empty;
+
+        private int _Status = 0;
+        private DateTime _LastUpdate = DateTime.MinValue;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public UserCredit() : base() { }
+
+        #endregion
+
+        #region Public Properties
+
+        #region Common
+
+        #endregion
+
+        #region TSB
+
+        /// <summary>
+        /// Gets or sets TSBId.
+        /// </summary>
+        [MaxLength(10)]
+        [PeropertyMapName("TSBId")]
+        public string TSBId
+        {
+            get
+            {
+                return _TSBId;
+            }
+            set
+            {
+                if (_TSBId != value)
+                {
+                    _TSBId = value;
+                    this.RaiseChanged("TSBId");
+                }
+            }
+        }
+        /// <summary>
+        /// Gets or sets TSBNameEN.
+        /// </summary>
+        [Ignore]
+        [PeropertyMapName("TSBNameEN")]
+        public virtual string TSBNameEN
+        {
+            get
+            {
+                return _TSBNameEN;
+            }
+            set
+            {
+                if (_TSBNameEN != value)
+                {
+                    _TSBNameEN = value;
+                    this.RaiseChanged("TSBNameEN");
+                }
+            }
+        }
+        /// <summary>
+        /// Gets or sets TSBNameTH.
+        /// </summary>
+        [Ignore]
+        [PeropertyMapName("TSBNameTH")]
+        public virtual string TSBNameTH
+        {
+            get
+            {
+                return _TSBNameTH;
+            }
+            set
+            {
+                if (_TSBNameTH != value)
+                {
+                    _TSBNameTH = value;
+                    this.RaiseChanged("TSBNameTH");
+                }
+            }
+        }
+
+        #endregion
+
+        #region User
+
+        /// <summary>
+        /// Gets or sets UserId
+        /// </summary>
+        [MaxLength(10)]
+        [PeropertyMapName("UserId")]
+        public string UserId
+        {
+            get
+            {
+                return _UserId;
+            }
+            set
+            {
+                if (_UserId != value)
+                {
+                    _UserId = value;
+                    this.RaiseChanged("UserId");
+                }
+            }
+        }
+        /// <summary>
+        /// Gets or sets FullNameEN
+        /// </summary>
+        [Ignore]
+        [PeropertyMapName("FullNameEN")]
+        public virtual string FullNameEN
+        {
+            get
+            {
+                return _FullNameEN;
+            }
+            set
+            {
+                if (_FullNameEN != value)
+                {
+                    _FullNameEN = value;
+                    this.RaiseChanged("FullNameEN");
+                }
+            }
+        }
+        /// <summary>
+        /// Gets or sets FullNameTH
+        /// </summary>
+        [Ignore]
+        [PeropertyMapName("FullNameTH")]
+        public virtual string FullNameTH
+        {
+            get
+            {
+                return _FullNameTH;
+            }
+            set
+            {
+                if (_FullNameTH != value)
+                {
+                    _FullNameTH = value;
+                    this.RaiseChanged("FullNameTH");
+                }
+            }
+        }
+
+        #endregion
+
+        #region Status (DC)
+
+        /// <summary>
+        /// Gets or sets Status (1 = Sync, 0 = Unsync, etc..)
+        /// </summary>
+        [PeropertyMapName("Status")]
+        public int Status
+        {
+            get
+            {
+                return _Status;
+            }
+            set
+            {
+                if (_Status != value)
+                {
+                    _Status = value;
+                    this.RaiseChanged("Status");
+                }
+            }
+        }
+        /// <summary>
+        /// Gets or sets LastUpdated (Sync to DC).
+        /// </summary>
+        [PeropertyMapName("LastUpdate")]
+        public DateTime LastUpdate
+        {
+            get { return _LastUpdate; }
+            set
+            {
+                if (_LastUpdate != value)
+                {
+                    _LastUpdate = value;
+                    this.RaiseChanged("LastUpdate");
+                }
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Internal Class
+
+        public class FKs : UserCredit
+        {
+            #region TSB
+
+            /// <summary>
+            /// Gets or sets TSBNameEN.
+            /// </summary>
+            [MaxLength(100)]
+            [PeropertyMapName("TSBNameEN")]
+            public override string TSBNameEN
+            {
+                get { return base.TSBNameEN; }
+                set { base.TSBNameEN = value; }
+            }
+            /// <summary>
+            /// Gets or sets TSBNameTH.
+            /// </summary>
+            [MaxLength(100)]
+            [PeropertyMapName("TSBNameTH")]
+            public override string TSBNameTH
+            {
+                get { return base.TSBNameTH; }
+                set { base.TSBNameTH = value; }
+            }
+
+            #endregion
+
+            #region User
+
+            /// <summary>
+            /// Gets or sets FullNameEN
+            /// </summary>
+            [MaxLength(100)]
+            [PeropertyMapName("FullNameEN")]
+            public override string FullNameEN
+            {
+                get { return base.FullNameEN; }
+                set { base.FullNameEN = value; }
+            }
+            /// <summary>
+            /// Gets or sets FullNameTH
+            /// </summary>
+            [MaxLength(100)]
+            [PeropertyMapName("FullNameTH")]
+            public override string FullNameTH
+            {
+                get { return base.FullNameTH; }
+                set { base.FullNameTH = value; }
+            }
+
+            #endregion
+
+            #region Public Methods
+
+            public UserCredit ToUserCredit()
+            {
+                UserCredit inst = new UserCredit();
+                this.AssignTo(inst); // set all properties to new instance.
+                return inst;
+            }
+
+            #endregion
+        }
+
+        #endregion
+
+        #region Static Methods
+
+        #endregion
+    }
+
+    #endregion
+}
