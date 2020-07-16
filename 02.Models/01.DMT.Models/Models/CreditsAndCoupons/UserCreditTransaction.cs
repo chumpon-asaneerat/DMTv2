ee0eca74ee31a83316482d8a9c81109bac56a93a
chumpon-asaneerat <chumponsenate@gmail.com>
2020-07-16 19:43:34 +0700

    Update 2020-07-16 #25 (TA) - Implements TA model class #15.   - Add TSBCreditTransaction class.   - Add UserCreditTransaction class.

---
+++ 02.Models/01.DMT.Models/Models/CreditsAndCoupons/UserCreditTransaction.cs
@@ -0,0 +1,24 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+using SQLiteNetExtensions.Extensions;
+
+// required for JsonIgnore.
+using Newtonsoft.Json;
+using NLib;
+using NLib.Reflection;
+using System.ComponentModel;
+
+#endregion
+
+namespace DMT.Models
+{
+    class UserCreditTransaction
+    {
+    }
+}
