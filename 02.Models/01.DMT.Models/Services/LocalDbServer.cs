2b3f227792c003ad17bc8f36605076e8674a7fb1
chumpon-asaneerat <chumponsenate@gmail.com>
2020-07-16 14:56:44 +0700

    Update 2020-07-16 #12 - Implements TA model class #10.   - Temporary disable Table creation for TSB Credit process.

--- 02.Models/01.DMT.Models/Services/LocalDbServer.cs
+++ 02.Models/01.DMT.Models/Services/LocalDbServer.cs
@@ -150,8 +150,8 @@ namespace DMT.Services
 
             Db.CreateTable<RevenueEntry>();
 
-            Db.CreateTable<TSBBalance>();
-            Db.CreateTable<UserCredit>();
+            //Db.CreateTable<TSBBalance>();
+            //Db.CreateTable<UserCredit>();
             //Db.CreateTable<TSBCredit>();
 
             InitDefaults();
