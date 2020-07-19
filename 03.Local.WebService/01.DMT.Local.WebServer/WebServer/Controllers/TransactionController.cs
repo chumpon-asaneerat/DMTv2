4d8f08fc9caffbd73d2cc914ed75d54ad4cedb53
chumpon-asaneerat <chumponsenate@gmail.com>
2020-07-19 11:40:33 +0700

    Update 2020-07-19 #34 (TA) - Implements TA/TOD model class #83.   - Implements Transaction Controller (Credit/Coupon/Addition) #1.   - Implements Plaza Operations Transaction (Credit/Coupon/Addition) #1.

---
+++ 03.Local.WebService/01.DMT.Local.WebServer/WebServer/Controllers/TransactionController.cs
@@ -0,0 +1,56 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+using DMT.Models;
+using DMT.Models.ExtensionMethods;
+
+#endregion
+
+namespace DMT.Services
+{
+    /// <summary>
+    /// The controller for manage credit transaction TA.
+    /// </summary>
+    [RoutePrefix("api/transaction/credit")]
+    public class CreditController : ApiController
+    {
+        [HttpPost]
+        [ActionName(RouteConsts.Transaction.Credit.GetInitial.Name)]
+        public TSBCreditTransaction GetInitial()
+        {
+            return TSBCreditTransaction.GetInitial();
+        }
+    }
+
+    /// <summary>
+    /// The controller for manage coupon transaction TA.
+    /// </summary>
+    [RoutePrefix("api/transaction/coupon")]
+    public class CouponController : ApiController
+    {
+        [HttpPost]
+        [ActionName(RouteConsts.Transaction.Coupon.GetInitial.Name)]
+        public TSBCouponTransaction GetInitial()
+        {
+            return TSBCouponTransaction.GetInitial();
+        }
+    }
+
+    /// <summary>
+    /// The controller for manage addition transaction TA.
+    /// </summary>
+    [RoutePrefix("api/transaction/addition")]
+    public class AdditionController : ApiController
+    {
+        [HttpPost]
+        [ActionName(RouteConsts.Transaction.Addition.GetInitial.Name)]
+        public TSBAdditionTransaction GetInitial()
+        {
+            return TSBAdditionTransaction.GetInitial();
+        }
+    }
+}
