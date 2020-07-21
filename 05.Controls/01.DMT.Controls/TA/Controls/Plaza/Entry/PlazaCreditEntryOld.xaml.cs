d727a4360b2d43f5ce51bb0bf22360b1dfdc6f8e
vorakanok <vorakanok@yahoo.com>
2020-07-21 14:50:23 +0700

    Update PlazaCreditEntry

---
+++ 05.Controls/01.DMT.Controls/TA/Controls/Plaza/Entry/PlazaCreditEntryOld.xaml.cs
@@ -0,0 +1,43 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+#endregion
+
+namespace DMT.TA.Controls.Plaza.Entry
+{
+    /// <summary>
+    /// Interaction logic for PlazaCreditEntryOld.xaml
+    /// </summary>
+    public partial class PlazaCreditEntryOld : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public PlazaCreditEntryOld()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Loaded/Unloaded
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+    }
+}
