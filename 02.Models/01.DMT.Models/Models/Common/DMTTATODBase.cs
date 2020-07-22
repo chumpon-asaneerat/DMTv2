f0bab54857f3421fe2e96b61935a4a0e766d5aab
chumpon-asaneerat <chumponsenate@gmail.com>
2020-07-22 10:52:50 +0700

    Update 2020-07-22 #1 (TA) - Implements TA/TOD model class #114   - Add DMTDataCenterBase<T> class.   - Add DMTTAEntryTable<T> class.

---
+++ 02.Models/01.DMT.Models/Models/Common/DMTTATODBase.cs
@@ -0,0 +1,44 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+using SQLiteNetExtensions.Extensions;
+
+// required for JsonIgnore.
+using Newtonsoft.Json;
+using NLib;
+using NLib.Reflection;
+using System.ComponentModel;
+
+#endregion
+
+namespace DMT.Models
+{
+    public abstract class DMTDataCenterBase<T> : NTable<T>
+        where T : NTable, new()
+    {
+        #region Internal Variables
+
+        #endregion
+
+        #region Public Properties
+
+        #endregion
+    }
+
+    public abstract class DMTTAEntryTable<T> : DMTDataCenterBase<T>
+        where T : NTable, new()
+    {
+        #region Internal Variables
+
+        #endregion
+
+        #region Public Properties
+
+        #endregion
+    }
+}
