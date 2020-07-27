72d09b0c32642fa57f41a639b5c37bf238310606
chumpon-asaneerat <chumponsenate@gmail.com>
2020-07-27 19:06:12 +0700

    Update 2020-07-27 #7 - Update DataCenterModelBase<T> class #2.

--- 02.Models/01.DMT.Models/Models/Common/ModelBaseClasses.cs
+++ 02.Models/01.DMT.Models/Models/Common/ModelBaseClasses.cs
@@ -1,8 +1,14 @@
 ﻿#region Using
 
+using System;
 using System.ComponentModel;
 using NLib;
+using NLib.Design;
+using NLib.Reflection;
 
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+using SQLiteNetExtensions.Extensions;
 // required for JsonIgnore attribute.
 using Newtonsoft.Json;
 using Newtonsoft.Json.Bson;
@@ -17,9 +23,68 @@ namespace DMT.Models
     /// DataCenterModelBase (abstract).
     /// </summary>
     /// <typeparam name="T">The Target Class.</typeparam>
+	[TypeConverter(typeof(PropertySorterSupportExpandableTypeConverter))]
     public abstract class DataCenterModelBase<T> : NTable<T>
         where T : NTable, new()
     {
+        #region Intenral Variables
+
+        private int _Status = 0;
+        private DateTime _LastUpdate = DateTime.MinValue;
+
+        #endregion
+
+        #region Public Proprties
+
+        #region Status (DC)
+
+        /// <summary>
+        /// Gets or sets Status (1 = Sync, 0 = Unsync, etc..)
+        /// </summary>
+        [Category("DataCenter")]
+        [Description("Gets or sets Status (1 = Sync, 0 = Unsync, etc..)")]
+        [ReadOnly(true)]
+        [PeropertyMapName("Status")]
+        [PropertyOrder(101)]
+        public int Status
+        {
+            get
+            {
+                return _Status;
+            }
+            set
+            {
+                if (_Status != value)
+                {
+                    _Status = value;
+                    this.RaiseChanged("Status");
+                }
+            }
+        }
+        /// <summary>
+        /// Gets or sets LastUpdated (Sync to DC).
+        /// </summary>
+        [Category("DataCenter")]
+        [Description("Gets or sets LastUpdated (Sync to DC).")]
+        [ReadOnly(true)]
+        [PeropertyMapName("LastUpdate")]
+        [PropertyOrder(102)]
+        public DateTime LastUpdate
+        {
+            get { return _LastUpdate; }
+            set
+            {
+                if (_LastUpdate != value)
+                {
+                    _LastUpdate = value;
+                    this.RaiseChanged("LastUpdate");
+                }
+            }
+        }
+
+        #endregion
+
+        #endregion
     }
 
     #endregion
@@ -30,9 +95,17 @@ namespace DMT.Models
     /// TSBModelBase (abstract).
     /// </summary>
     /// <typeparam name="T">The Target Class.</typeparam>
+	[TypeConverter(typeof(PropertySorterSupportExpandableTypeConverter))]
     public abstract class TSBModelBase<T> : DataCenterModelBase<T>
         where T : NTable, new()
     {
+        #region Intenral Variables
+
+        #endregion
+
+        #region Public Proprties
+
+        #endregion
     }
 
     #endregion
