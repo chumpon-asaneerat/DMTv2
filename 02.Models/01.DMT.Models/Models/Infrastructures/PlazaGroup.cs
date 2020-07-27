88dcac3e2ce68fd7be28d9a11ee9778bc0ce0795
chumpon-asaneerat <chumponsenate@gmail.com>
2020-07-27 19:44:41 +0700

    Update 2020-07-27 #17 - Update PlazaGroup class #3.

--- 02.Models/01.DMT.Models/Models/Infrastructures/PlazaGroup.cs
+++ 02.Models/01.DMT.Models/Models/Infrastructures/PlazaGroup.cs
@@ -24,7 +24,7 @@ namespace DMT.Models
     /// </summary>
     [TypeConverter(typeof(PropertySorterSupportExpandableTypeConverter))]
     //[Table("PlazaGroup")]
-    public class PlazaGroup : TSBModelBase<PlazaGroup>
+    public class PlazaGroup : PlazaGroupModelBase<PlazaGroup>
     {
         #region Public Properties
 
@@ -33,25 +33,37 @@ namespace DMT.Models
         /// </summary>
         [PropertyOrder(201)]
         public override string PlazaGroupId
-        { }
+        {
+            get { return base.PlazaGroupId; }
+            set { base.PlazaGroupId = value; }
+        }
         /// <summary>
         /// Gets or sets PlazaGroupNameEN
         /// </summary>
         [PropertyOrder(202)]
         public override string PlazaGroupNameEN
-        { }
+        {
+            get { return base.PlazaGroupNameEN; }
+            set { base.PlazaGroupNameEN = value; }
+        }
         /// <summary>
         /// Gets or sets PlazaGroupNameTH
         /// </summary>
         [PropertyOrder(203)]
         public override string PlazaGroupNameTH
-        { }
+        {
+            get { return base.PlazaGroupNameTH; }
+            set { base.PlazaGroupNameTH = value; }
+        }
         /// <summary>
         /// Gets or sets Direction
         /// </summary>
         [PropertyOrder(204)]
         public override string Direction
-        { }
+        {
+            get { return base.Direction; }
+            set { base.Direction = value; }
+        }
 
         #endregion
     }
