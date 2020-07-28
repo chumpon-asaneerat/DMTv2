81df211446978b4ded93590d4c92c4526e4f9b55
chumpon-asaneerat <chumponsenate@gmail.com>
2020-07-28 20:03:04 +0700

    [commited] Update 2020-07-28 #3 - Add Lane.cs

---
+++ 02.Models/01.DMT.Models/Models/Infrastructures/Lane.cs
@@ -0,0 +1,30 @@
+﻿#region Using
+
+using System;
+using System.ComponentModel;
+using NLib;
+using NLib.Design;
+using NLib.Reflection;
+
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+using SQLiteNetExtensions.Extensions;
+// required for JsonIgnore attribute.
+using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
+
+#endregion
+
+namespace DMT.Models
+{
+	#region TSB
+
+	/// <summary>
+	/// The TSB Data Model class.
+	/// </summary>
+	[TypeConverter(typeof(PropertySorterSupportExpandableTypeConverter))]
+	//[Table("TSB")]
+	class Lane
+	{
+	}
+}
