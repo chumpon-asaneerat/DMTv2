13d177c9895512cb0cc20682edd431cf60b3b8e9
chumpon-asaneerat <chumponsenate@gmail.com>
2020-07-28 20:05:20 +0700

    Update 2020-07-28 #5 - Add User.cs

---
+++ 02.Models/01.DMT.Models/Models/Users/User.cs
@@ -0,0 +1,23 @@
+﻿#region Using
+
+using System;
+using System.ComponentModel;
+using NLib;
+using NLib.Design;
+using NLib.Reflection;
+
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+using SQLiteNetExtensions.Extensions;
+// required for JsonIgnore attribute.
+using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
+
+#endregion
+
+namespace DMT.Models
+{
+    class User
+    {
+    }
+}
