dfdffd3061e91148d8eb3c36b025f70dc15d3832
chumpon-asaneerat <chumponsenate@gmail.com>
2020-07-31 16:51:29 +0700

    Update 2020-07-31 #3 - Add UserCoupon model. - Add UserCouponTransaction model.

---
+++ 02.Models/01.DMT.Models/Models/CreditsAndCoupons/Users/Coupons/UserCoupon.cs
@@ -0,0 +1,37 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+using NLib;
+using NLib.Design;
+using NLib.Reflection;
+
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+using SQLiteNetExtensions.Extensions;
+// required for JsonIgnore attribute.
+using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
+
+#endregion
+
+namespace DMT.Models
+{
+	#region UserCoupon
+
+	/// <summary>
+	/// The UserCredit Data Model class.
+	/// </summary>
+	[TypeConverter(typeof(PropertySorterSupportExpandableTypeConverter))]
+	[Serializable]
+	[JsonObject(MemberSerialization.OptOut)]
+	//[Table("UserCoupon")]
+	public class UserCoupon
+	{
+	}
+
+	#endregion
+}
