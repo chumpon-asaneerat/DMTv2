dca701a16c54c0b7e0481fa0677b7b43b0ac8d0c
chumpon-asaneerat <chumponsenate@gmail.com>
2020-07-31 16:56:45 +0700

    Update 2020-07-31 #5 - Update UserCoupon model #3. - Update UserCouponTransaction model #3.

--- 02.Models/01.DMT.Models/Models/CreditsAndCoupons/Users/Coupons/UserCoupon.cs
+++ 02.Models/01.DMT.Models/Models/CreditsAndCoupons/Users/Coupons/UserCoupon.cs
@@ -29,13 +29,16 @@ namespace DMT.Models
 	[Serializable]
 	[JsonObject(MemberSerialization.OptOut)]
 	//[Table("UserCoupon")]
-	public class UserCoupon
+	public class UserCoupon : NTable<UserCoupon>
 	{
 		#region Enum
 
 		#endregion
 
 		#region Internal Variables
+
+		private int _Status = 0;
+		private DateTime _LastUpdate = DateTime.MinValue;
 
 		#endregion
 
@@ -54,9 +57,66 @@ namespace DMT.Models
 
 		#region Public Properties
 
+		#region Common
+
+		#endregion
+
+		#region Status (DC)
+
+		/// <summary>
+		/// Gets or sets Status (1 = Sync, 0 = Unsync, etc..)
+		/// </summary>
+		[Category("DataCenter")]
+		[Description("Gets or sets Status (1 = Sync, 0 = Unsync, etc..)")]
+		[ReadOnly(true)]
+		[PeropertyMapName("Status")]
+		[PropertyOrder(10001)]
+		public int Status
+		{
+			get
+			{
+				return _Status;
+			}
+			set
+			{
+				if (_Status != value)
+				{
+					_Status = value;
+					this.RaiseChanged("Status");
+				}
+			}
+		}
+		/// <summary>
+		/// Gets or sets LastUpdated (Sync to DC).
+		/// </summary>
+		[Category("DataCenter")]
+		[Description("Gets or sets LastUpdated (Sync to DC).")]
+		[ReadOnly(true)]
+		[PeropertyMapName("LastUpdate")]
+		[PropertyOrder(10002)]
+		public DateTime LastUpdate
+		{
+			get { return _LastUpdate; }
+			set
+			{
+				if (_LastUpdate != value)
+				{
+					_LastUpdate = value;
+					this.RaiseChanged("LastUpdate");
+				}
+			}
+		}
+
+		#endregion
+
 		#endregion
 
 		#region Internal Class
+
+		public class FKs : UserCoupon
+		{
+
+        }
 
 		#endregion
 
