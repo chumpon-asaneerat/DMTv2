5388a143b3adab2981ea137a5b1db5ad5c47f1cd
chumpon-asaneerat <chumponsenate@gmail.com>
2020-08-01 13:30:51 +0700

    Update 2020-08-01 #18 - Update TSBCouponBalanceView.sql #3

--- 02.Models/01.DMT.Models/Views/Scripts/TSBCouponBalanceView.sql
+++ 02.Models/01.DMT.Models/Views/Scripts/TSBCouponBalanceView.sql
@@ -4,45 +4,69 @@ AS
 		 , TSB.TSBNameEN
 		 , TSB.TSBNameTH
 		 , ((
-			 SELECT IFNULL(SUM(CouponBHT35), 0) 
+			 SELECT IFNULL(COUNT(*), 0) 
 			   FROM TSBCouponTransaction 
 			  WHERE (   TSBCouponTransaction.TransactionType = 0 
 					 OR TSBCouponTransaction.TransactionType = 1
 					) -- Initial = 0, Received = 1
 				AND TSBCouponTransaction.TSBId = TSB.TSBId
+				AND TSBCouponTransactionCouponType = 35
 			) -
 			(
-			 SELECT IFNULL(SUM(CouponBHT35), 0) 
+			 SELECT IFNULL(COUNT(*), 0) 
 			   FROM TSBCouponTransaction 
 			  WHERE TSBCouponTransaction.TransactionType = 2 -- Sold = 2
 				AND TSBCouponTransaction.TSBId = TSB.TSBId
+				AND TSBCouponTransactionCouponType = 35
 			)) AS CouponBHT35
 		 , ((
-			 SELECT IFNULL(SUM(CouponBHT80), 0) 
+			 SELECT IFNULL(COUNT(*), 0) 
 			   FROM TSBCouponTransaction 
 			  WHERE (   TSBCouponTransaction.TransactionType = 0 
 					 OR TSBCouponTransaction.TransactionType = 1
 					) -- Initial = 0, Received = 1
 				AND TSBCouponTransaction.TSBId = TSB.TSBId
+				AND TSBCouponTransactionCouponType = 80
 			) -
 			(
-			 SELECT IFNULL(SUM(CouponBHT80), 0) 
+			 SELECT IFNULL(COUNT(*), 0) 
 			   FROM TSBCouponTransaction 
 			  WHERE TSBCouponTransaction.TransactionType = 2 -- Sold = 2
 				AND TSBCouponTransaction.TSBId = TSB.TSBId
+				AND TSBCouponTransactionCouponType = 80
 			)) AS CouponBHT80
 		 , ((
-			 SELECT IFNULL(SUM((CouponBHT35 * CouponBHT35Factor) + (CouponBHT80 * CouponBHT80Factor)), 0) 
+			 SELECT IFNULL(SUM(Factor), 0) 
 			   FROM TSBCouponTransaction 
 			  WHERE (   TSBCouponTransaction.TransactionType = 0 
 					 OR TSBCouponTransaction.TransactionType = 1
 					) -- Initial = 0, Received = 1
 				AND TSBCouponTransaction.TSBId = TSB.TSBId
+				AND TSBCouponTransactionCouponType = 35
+			) +
+			(
+			 SELECT IFNULL(SUM(Factor), 0) 
+			   FROM TSBCouponTransaction 
+			  WHERE (   TSBCouponTransaction.TransactionType = 0 
+					 OR TSBCouponTransaction.TransactionType = 1
+					) -- Initial = 0, Received = 1
+				AND TSBCouponTransaction.TSBId = TSB.TSBId
+				AND TSBCouponTransactionCouponType = 80
 			) -
 			(
-			 SELECT IFNULL(SUM((CouponBHT35 * CouponBHT35Factor) + (CouponBHT80 * CouponBHT80Factor)), 0) 
+			 SELECT IFNULL(SUM(Factor), 0) 
+			   FROM TSBCouponTransaction 
+			  WHERE (   TSBCouponTransaction.TransactionType = 0 
+					 OR TSBCouponTransaction.TransactionType = 1
+					) -- Initial = 0, Received = 1
+				AND TSBCouponTransaction.TSBId = TSB.TSBId
+				AND TSBCouponTransactionCouponType = 35
+			) -
+			(
+			 SELECT IFNULL(SUM(Factor), 0) 
 			   FROM TSBCouponTransaction 
 			  WHERE TSBCouponTransaction.TransactionType = 2 -- Sold = 2
 				AND TSBCouponTransaction.TSBId = TSB.TSBId
+				AND TSBCouponTransactionCouponType = 80
 			)) AS CouponBHTTotal
 	  FROM TSB
