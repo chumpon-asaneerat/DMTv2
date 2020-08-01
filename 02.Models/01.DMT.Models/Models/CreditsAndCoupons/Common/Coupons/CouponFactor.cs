94e2a7ae2170b7b12e43198e6379b51348b16500
chumpon-asaneerat <chumponsenate@gmail.com>
2020-08-01 12:06:14 +0700

    Update 2020-08-01 #5 - Add CouponFactor model class. - Add CouponType enum.

---
+++ 02.Models/01.DMT.Models/Models/CreditsAndCoupons/Common/Coupons/CouponFactor.cs
@@ -0,0 +1,67 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+using NLib;
+using NLib.Design;
+using NLib.Reflection;
+
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+using SQLiteNetExtensions.Extensions;
+// required for JsonIgnore attribute.
+using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
+
+#endregion
+
+namespace DMT.Models
+{
+    #region Enun
+
+    public enum CouponType : int
+    {
+        BHT35 = 35,
+        BHT80 = 80
+    }
+
+    #endregion
+
+    #region CouponFactor
+
+    /// <summary>
+    /// The CouponFactor Data Model class.
+    /// </summary>
+    [TypeConverter(typeof(PropertySorterSupportExpandableTypeConverter))]
+    [Serializable]
+    [JsonObject(MemberSerialization.OptOut)]
+    //[Table("CouponFactor")]
+    public class CouponFactor : NTable<CouponFactor>
+    {
+        #region Internal Variable
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public CouponFactor() : base() { }
+
+        #endregion
+
+        #region Public Properties
+
+        #endregion
+
+        #region Static Methods
+
+        #endregion
+    }
+
+    #endregion
+}
