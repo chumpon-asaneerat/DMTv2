823956ee9f3444d55ba80263de82601c41f1528a
chumpon-asaneerat <chumponsenate@gmail.com>
2020-08-01 12:33:37 +0700

    Update 2020-08-01 #10 - Update CouponFactor model class #2.

--- 02.Models/01.DMT.Models/Models/CreditsAndCoupons/Common/Coupons/CouponFactor.cs
+++ 02.Models/01.DMT.Models/Models/CreditsAndCoupons/Common/Coupons/CouponFactor.cs
@@ -43,6 +43,15 @@ namespace DMT.Models
     {
         #region Internal Variable
 
+        private int _PKId = 0;
+        private CouponType _CouponType = CouponType.BHT35;
+        private decimal _Factor = 665;
+        private DateTime _Begin = DateTime.MinValue;
+        private DateTime _End = DateTime.MinValue;
+
+        private int _Status = 0;
+        private DateTime _LastUpdate = DateTime.MinValue;
+
         #endregion
 
         #region Constructor
@@ -55,6 +64,268 @@ namespace DMT.Models
         #endregion
 
         #region Public Properties
+
+        #region Common
+
+        /// <summary>
+        /// Gets or sets PK Id.
+        /// </summary>
+        [Category("Coupon")]
+        [Description("Gets or sets PK Id.")]
+        [PrimaryKey, AutoIncrement]
+        [PeropertyMapName("PKId")]
+        public virtual int PKId
+        {
+            get { return _PKId; }
+            set
+            {
+                if (_PKId != value)
+                {
+                    _PKId = value;
+                    // Raise event.
+                    this.RaiseChanged("PKId");
+                }
+            }
+        }
+        /// <summary>
+        /// Gets or sets coupon type.
+        /// </summary>
+        [Category("Coupon")]
+        [Description("Gets or sets coupon type.")]
+        [PeropertyMapName("CouponType")]
+        public virtual CouponType CouponType
+        {
+            get { return _CouponType; }
+            set
+            {
+                if (_CouponType != value)
+                {
+                    _CouponType = value;
+                    // Raise event.
+                    this.RaiseChanged("CouponType");
+                }
+            }
+        }
+        /// <summary>
+        /// Gets or sets coupon factor.
+        /// </summary>
+        [Category("Coupon")]
+        [Description("Gets or sets coupon factor.")]
+        [PeropertyMapName("Factor")]
+        public virtual decimal Factor
+        {
+            get { return _Factor; }
+            set
+            {
+                if (_Factor != value)
+                {
+                    _Factor = value;
+                    // Raise event.
+                    this.RaiseChanged("Factor");
+                }
+            }
+        }
+
+        #endregion
+
+        #region Begin/End
+
+        /// <summary>
+        /// Gets or sets Begin Date.
+        /// </summary>
+        [Category("Effect Date")]
+        [Description("Gets or sets Begin Date.")]
+        //[ReadOnly(true)]
+        [PeropertyMapName("Begin")]
+        public DateTime Begin
+        {
+            get { return _Begin; }
+            set
+            {
+                if (_Begin != value)
+                {
+                    _Begin = value;
+                    // Raise event.
+                    RaiseChanged("Begin");
+                    RaiseChanged("BeginDateString");
+                    RaiseChanged("BeginTimeString");
+                    RaiseChanged("BeginDateTimeString");
+                }
+            }
+        }
+        /// <summary>
+        /// Gets or sets End Date.
+        /// </summary>
+        [Category("Effect Date")]
+        [Description("Gets or sets End Date.")]
+        //[ReadOnly(true)]
+        [PeropertyMapName("End")]
+        public DateTime End
+        {
+            get { return _End; }
+            set
+            {
+                if (_End != value)
+                {
+                    _End = value;
+                    // Raise event.
+                    RaiseChanged("End");
+                    RaiseChanged("EndDateString");
+                    RaiseChanged("EndTimeString");
+                    RaiseChanged("EndDateTimeString");
+                }
+            }
+        }
+        /// <summary>
+        /// Gets Begin Date String.
+        /// </summary>
+        [Category("Effect Date")]
+        [Description("Gets Begin Date String.")]
+        [ReadOnly(true)]
+        [JsonIgnore]
+        [Ignore]
+        public string BeginDateString
+        {
+            get
+            {
+                var ret = (this.Begin == DateTime.MinValue) ? "" : this.Begin.ToThaiDateTimeString("dd/MM/yyyy");
+                return ret;
+            }
+            set { }
+        }
+        /// <summary>
+        /// Gets End Date String.
+        /// </summary>
+        [Category("Effect Date")]
+        [Description("Gets End Date String.")]
+        [ReadOnly(true)]
+        [JsonIgnore]
+        [Ignore]
+        public string EndDateString
+        {
+            get
+            {
+                var ret = (this.End == DateTime.MinValue) ? "" : this.End.ToThaiDateTimeString("dd/MM/yyyy");
+                return ret;
+            }
+            set { }
+        }
+        /// <summary>
+        /// Gets Begin Time String.
+        /// </summary>
+        [Category("Effect Date")]
+        [Description("Gets Begin Time String.")]
+        [ReadOnly(true)]
+        [JsonIgnore]
+        [Ignore]
+        public string BeginTimeString
+        {
+            get
+            {
+                var ret = (this.Begin == DateTime.MinValue) ? "" : this.Begin.ToThaiTimeString();
+                return ret;
+            }
+            set { }
+        }
+        /// <summary>
+        /// Gets End Time String.
+        /// </summary>
+        [Category("Effect Date")]
+        [Description("Gets End Time String.")]
+        [ReadOnly(true)]
+        [JsonIgnore]
+        [Ignore]
+        public string EndTimeString
+        {
+            get
+            {
+                var ret = (this.End == DateTime.MinValue) ? "" : this.End.ToThaiTimeString();
+                return ret;
+            }
+            set { }
+        }
+        /// <summary>
+        /// Gets or sets Begin Date Time String..
+        /// </summary>
+        [Category("Effect Date")]
+        [Description("Gets or sets Begin Date Time String.")]
+        [ReadOnly(true)]
+        [JsonIgnore]
+        [Ignore]
+        public string BeginDateTimeString
+        {
+            get
+            {
+                var ret = (this.Begin == DateTime.MinValue) ? "" : this.Begin.ToThaiDateTimeString("dd/MM/yyyy HH:mm:ss");
+                return ret;
+            }
+            set { }
+        }
+        /// <summary>
+        /// Gets End Date Time String.
+        /// </summary>
+        [Category("Effect Date")]
+        [Description("Gets End Date Time String.")]
+        [ReadOnly(true)]
+        [JsonIgnore]
+        [Ignore]
+        public string EndDateTimeString
+        {
+            get
+            {
+                var ret = (this.End == DateTime.MinValue) ? "" : this.End.ToThaiDateTimeString("dd/MM/yyyy HH:mm:ss");
+                return ret;
+            }
+            set { }
+        }
+
+        #endregion
+
+        #region Status (DC)
+
+        /// <summary>
+        /// Gets or sets Status (1 = Sync, 0 = Unsync, etc..)
+        /// </summary>
+        [Category("DataCenter")]
+        [Description("Gets or sets Status (1 = Sync, 0 = Unsync, etc..)")]
+        [ReadOnly(true)]
+        [PeropertyMapName("Status")]
+        public int Status
+        {
+            get
+            {
+                return _Status;
+            }
+            set
+            {
+                if (_Status != value)
+                {
+                    _Status = value;
+                    this.RaiseChanged("Status");
+                }
+            }
+        }
+        /// <summary>
+        /// Gets or sets LastUpdated (Sync to DC).
+        /// </summary>
+        [Category("DataCenter")]
+        [Description("Gets or sets LastUpdated (Sync to DC).")]
+        [ReadOnly(true)]
+        [PeropertyMapName("LastUpdate")]
+        public DateTime LastUpdate
+        {
+            get { return _LastUpdate; }
+            set
+            {
+                if (_LastUpdate != value)
+                {
+                    _LastUpdate = value;
+                    this.RaiseChanged("LastUpdate");
+                }
+            }
+        }
+
+        #endregion
 
         #endregion
 
--- 02.Models/01.DMT.Models/Services/LocalDbServer.cs
+++ 02.Models/01.DMT.Models/Services/LocalDbServer.cs
@@ -144,6 +144,7 @@ namespace DMT.Services
 			Db.CreateTable<User>();
 
 			Db.CreateTable<Payment>();
+			Db.CreateTable<CouponFactor>();
 
 			Db.CreateTable<Config>();
 			Db.CreateTable<ViewHistory>();
