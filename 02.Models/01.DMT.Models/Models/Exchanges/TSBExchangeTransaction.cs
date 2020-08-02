95dfd4a26f8d2f0dea380365c4a0a3506dc55ccc
chumpon-asaneerat <chumponsenate@gmail.com>
2020-08-02 22:39:23 +0700

    Update 2020-08-02 #2 (TA) - Implements Request Exchange process #1.

---
+++ 02.Models/01.DMT.Models/Models/Exchanges/TSBExchangeTransaction.cs
@@ -0,0 +1,279 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+using NLib;
+using NLib.Design;
+using NLib.Reflection;
+
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+using SQLiteNetExtensions.Extensions;
+// required for JsonIgnore attribute.
+using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
+
+#endregion
+
+namespace DMT.Models
+{
+	#region TSBExchangeTransaction
+
+	/// <summary>
+	/// The TSBExchangeTransaction Data Model class.
+	/// </summary>
+	[TypeConverter(typeof(PropertySorterSupportExpandableTypeConverter))]
+	[Serializable]
+	[JsonObject(MemberSerialization.OptOut)]
+	//[Table("TSBExchangeTransaction")]
+	public class TSBExchangeTransaction : NTable<TSBExchangeTransaction>
+	{
+		#region Enum
+
+		public enum TransactionTypes : int
+		{
+			// Request.
+			Request = 1,
+			// Approve.
+			Approve = 2,
+			// Received.
+			Received = 3,
+			// Canceled.
+			Canceled = 4
+		}
+
+		#endregion
+
+		#region Internal Variables
+
+		private int _TransactionId = 0;
+		private DateTime _TransactionDate = DateTime.MinValue;
+		private TransactionTypes _TransactionType = TransactionTypes.Request;
+
+		private string _TSBId = string.Empty;
+		private string _TSBNameEN = string.Empty;
+		private string _TSBNameTH = string.Empty;
+
+		#endregion
+
+		#region Constructor
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public TSBExchangeTransaction() : base() { }
+
+		#endregion
+
+		#region Public Properties
+
+		#region Common
+
+		/// <summary>
+		/// Gets or sets TransactionId
+		/// </summary>
+		[Category("Common")]
+		[Description(" Gets or sets TransactionId")]
+		[ReadOnly(true)]
+		[PrimaryKey, AutoIncrement]
+		[PeropertyMapName("TransactionId")]
+		public int TransactionId
+		{
+			get
+			{
+				return _TransactionId;
+			}
+			set
+			{
+				if (_TransactionId != value)
+				{
+					_TransactionId = value;
+					this.RaiseChanged("TransactionId");
+				}
+			}
+		}
+		/// <summary>
+		/// Gets or sets Transaction Date.
+		/// </summary>
+		[Category("Common")]
+		[Description(" Gets or sets Transaction Date")]
+		[ReadOnly(true)]
+		[PeropertyMapName("TransactionDate")]
+		public DateTime TransactionDate
+		{
+			get
+			{
+				return _TransactionDate;
+			}
+			set
+			{
+				if (_TransactionDate != value)
+				{
+					_TransactionDate = value;
+					this.RaiseChanged("TransactionDate");
+				}
+			}
+		}
+		/// <summary>
+		/// Gets Transaction Date String.
+		/// </summary>
+		[Category("Common")]
+		[Description("Gets Transaction Date String.")]
+		[ReadOnly(true)]
+		[JsonIgnore]
+		[Ignore]
+		public string TransactionDateString
+		{
+			get
+			{
+				var ret = (this.TransactionDate == DateTime.MinValue) ? "" : this.TransactionDate.ToThaiDateTimeString("dd/MM/yyyy");
+				return ret;
+			}
+			set { }
+		}
+		/// <summary>
+		/// Gets Transaction Time String.
+		/// </summary>
+		[Category("Common")]
+		[Description("Gets Transaction Time String.")]
+		[ReadOnly(true)]
+		[JsonIgnore]
+		[Ignore]
+		public string TransactionTimeString
+		{
+			get
+			{
+				var ret = (this.TransactionDate == DateTime.MinValue) ? "" : this.TransactionDate.ToThaiTimeString();
+				return ret;
+			}
+			set { }
+		}
+		/// <summary>
+		/// Gets Transaction Date Time String.
+		/// </summary>
+		[Category("Common")]
+		[Description("Gets Transaction Date Time String.")]
+		[ReadOnly(true)]
+		[JsonIgnore]
+		[Ignore]
+		public string TransactionDateTimeString
+		{
+			get
+			{
+				var ret = (this.TransactionDate == DateTime.MinValue) ? "" : this.TransactionDate.ToThaiDateTimeString("dd/MM/yyyy HH:mm:ss");
+				return ret;
+			}
+			set { }
+		}
+		/// <summary>
+		/// Gets or sets Transaction Type.
+		/// </summary>
+		[Category("Common")]
+		[Description("Gets or sets Transaction Type.")]
+		[ReadOnly(true)]
+		[PeropertyMapName("TransactionType")]
+		public TransactionTypes TransactionType
+		{
+			get { return _TransactionType; }
+			set
+			{
+				if (_TransactionType != value)
+				{
+					_TransactionType = value;
+					this.RaiseChanged("TransactionType");
+				}
+			}
+		}
+
+		#endregion
+
+		#region TSB
+
+		/// <summary>
+		/// Gets or sets TSBId.
+		/// </summary>
+		[Category("TSB")]
+		[Description("Gets or sets TSBId.")]
+		[ReadOnly(true)]
+		[MaxLength(10)]
+		[PeropertyMapName("TSBId")]
+		public string TSBId
+		{
+			get
+			{
+				return _TSBId;
+			}
+			set
+			{
+				if (_TSBId != value)
+				{
+					_TSBId = value;
+					this.RaiseChanged("TSBId");
+				}
+			}
+		}
+		/// <summary>
+		/// Gets or sets TSBNameEN.
+		/// </summary>
+		[Category("TSB")]
+		[Description("Gets or sets TSBNameEN.")]
+		[ReadOnly(true)]
+		[Ignore]
+		[PeropertyMapName("TSBNameEN")]
+		public virtual string TSBNameEN
+		{
+			get
+			{
+				return _TSBNameEN;
+			}
+			set
+			{
+				if (_TSBNameEN != value)
+				{
+					_TSBNameEN = value;
+					this.RaiseChanged("TSBNameEN");
+				}
+			}
+		}
+		/// <summary>
+		/// Gets or sets TSBNameTH.
+		/// </summary>
+		[Category("TSB")]
+		[Description("Gets or sets TSBNameTH.")]
+		[ReadOnly(true)]
+		[Ignore]
+		[PeropertyMapName("TSBNameTH")]
+		public virtual string TSBNameTH
+		{
+			get
+			{
+				return _TSBNameTH;
+			}
+			set
+			{
+				if (_TSBNameTH != value)
+				{
+					_TSBNameTH = value;
+					this.RaiseChanged("TSBNameTH");
+				}
+			}
+		}
+
+		#endregion
+
+		#endregion
+
+		#region Internal Class
+
+		#endregion
+
+		#region Static Methods
+
+		#endregion
+	}
+
+	#endregion
+}
