a05b1a48dfc86cf2d86497b12b0d6798cca97168
chumpon-asaneerat <chumponsenate@gmail.com>
2020-08-02 23:12:25 +0700

    Update 2020-08-02 #5 (TA) - Implements Request Exchange process #4.

---
+++ 10.Tests/05.LocalDbServerSample/Form1.cs
@@ -0,0 +1,46 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using NLib;
+using DMT.Models;
+using DMT.Services;
+
+#endregion
+
+namespace LocalDbServerSample
+{
+    public partial class Form1 : Form
+    {
+        #region Constructor
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Load/Closing
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LocalDbServer.Instance.Start();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            LocalDbServer.Instance.Shutdown();
+        }
+
+        #endregion
+    }
+}
