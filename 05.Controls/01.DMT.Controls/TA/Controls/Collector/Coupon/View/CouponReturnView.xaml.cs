a7c17c79d1fb086df70531da8d203e001438b041
chumpon-asaneerat <chumponsenate@gmail.com>
2020-08-06 11:41:27 +0700

    Update 2020-08-06 #5 (TA) - Update Coupon related process #67.

--- 05.Controls/01.DMT.Controls/TA/Controls/Collector/Coupon/View/CouponReturnView.xaml.cs
+++ 05.Controls/01.DMT.Controls/TA/Controls/Collector/Coupon/View/CouponReturnView.xaml.cs
@@ -1,7 +1,22 @@
-﻿using System;
+﻿#region Using
+
+using System;
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
+
+using DMT.Models;
+using DMT.Services;
+using NLib.Services;
+using NLib.Reflection;
+using NLib.Reports.Rdlc;
+using System.Reflection;
+using System.ComponentModel;
+using System.Windows.Interop;
+using NLib;
+using System.Windows.Threading;
+
+#endregion
 
 namespace DMT.TA.Controls.Collector.Coupon
 {
@@ -22,16 +37,18 @@ namespace DMT.TA.Controls.Collector.Coupon
 
         #endregion
 
+        private PlazaOperations ops = DMTServiceOperations.Instance.Plaza;
+
         private void cmdReturn_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        /*
-        public void Setup(List<Models.Coupon> coupons)
+        public void RefreshList()
         {
+            // need to change get coupon on lanes instead.
+            var coupons = ops.Coupons.GetTSBCouponSummaries(null);
             listView.ItemsSource = coupons;
         }
-        */
     }
 }
