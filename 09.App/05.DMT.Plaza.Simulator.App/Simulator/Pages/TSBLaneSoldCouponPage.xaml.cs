1384bbd2375cc03d1f0716743ed9232a892c98c7
chumpon-asaneerat <chumponsenate@gmail.com>
2020-08-06 12:54:11 +0700

    Update 2020-08-06 #13 - Update Coupon related process #76.

---
+++ 09.App/05.DMT.Plaza.Simulator.App/Simulator/Pages/TSBLaneSoldCouponPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using DMT.Models;
+using DMT.Services;
+using System.Collections.ObjectModel;
+using NLib.Reflection;
+using Xceed.Wpf.Toolkit.PropertyGrid.Attributes;
+using System.Runtime.InteropServices;
+
+#endregion
+
+
+namespace DMT.Simulator.Pages
+{
+    /// <summary>
+    /// Interaction logic for TSBLaneSoldCouponPage.xaml
+    /// </summary>
+    public partial class TSBLaneSoldCouponPage : UserControl
+    {
+        #region Constructor
+
+        public TSBLaneSoldCouponPage()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Loaded/Unloaded
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+    }
+}
