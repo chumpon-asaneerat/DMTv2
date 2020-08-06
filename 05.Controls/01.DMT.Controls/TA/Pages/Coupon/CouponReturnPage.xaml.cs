bf904620fd5c1894109663529294b809d7af4417
chumpon-asaneerat <chumponsenate@gmail.com>
2020-08-06 19:43:46 +0700

    Update 2020-08-06 #32 - Update Coupon related process #87.

--- 05.Controls/01.DMT.Controls/TA/Pages/Coupon/CouponReturnPage.xaml.cs
+++ 05.Controls/01.DMT.Controls/TA/Pages/Coupon/CouponReturnPage.xaml.cs
@@ -34,6 +34,23 @@ namespace DMT.TA.Pages.Coupon
 
         #endregion
 
+        private PlazaOperations ops = DMTServiceOperations.Instance.Plaza;
+        private TSBCouponManager manager = new TSBCouponManager();
+
+        #region Loaded/Unloaded
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+
         #region Button
 
         private void cmdCancel_Click(object sender, RoutedEventArgs e)
@@ -81,5 +98,56 @@ namespace DMT.TA.Pages.Coupon
         }
 
         #endregion
+
+        #region Filter Handlers
+
+        private void txtFilter35_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+
+        }
+
+        private void txtFilter80_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        public void Setup(User user)
+        {
+            manager.User = user;
+            LoadCoupons();
+        }
+
+        private void LoadCoupons()
+        {
+            manager.Refresh();
+
+            RefreshBHT35Coupons();
+            RefreshBHT80Coupons();
+        }
+
+        private void RefreshBHT35Coupons()
+        {
+            lvTSB35.ItemsSource = null;
+            lvTSB35.ItemsSource = manager.C35Stocks.FindAll(item =>
+            {
+                return item.CouponId.Contains(txtFilter35.Text) && item.TransactionType == TSBCouponTransaction.TransactionTypes.Stock;
+            });
+
+            lvSold35.ItemsSource = null;
+            lvSold35.ItemsSource = manager.C35Users;
+        }
+
+        private void RefreshBHT80Coupons()
+        {
+            lvTSB80.ItemsSource = null;
+            lvTSB80.ItemsSource = manager.C80Stocks.FindAll(item =>
+            {
+                return item.CouponId.Contains(txtFilter80.Text) && item.TransactionType == TSBCouponTransaction.TransactionTypes.Stock;
+            });
+            lvSold80.ItemsSource = null;
+            lvSold80.ItemsSource = manager.C80Users;
+        }
     }
 }
