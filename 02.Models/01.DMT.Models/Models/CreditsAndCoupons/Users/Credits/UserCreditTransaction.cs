6a8aec5c4fcbca7481efcdf1b9868fb6a283ee14
chumpon-asaneerat <chumponsenate@gmail.com>
2020-08-07 21:19:02 +0700

    Update 2020-08-07 #14 - Update Exchange process #10.

--- 02.Models/01.DMT.Models/Models/CreditsAndCoupons/Users/Credits/UserCreditTransaction.cs
+++ 02.Models/01.DMT.Models/Models/CreditsAndCoupons/Users/Credits/UserCreditTransaction.cs
@@ -32,9 +32,6 @@ namespace DMT.Models
 	//[Table("UserCredit")]
 	public class UserCreditTransaction : NTable<UserCreditTransaction>
 	{
-		private static SolidColorBrush RedForeground = new SolidColorBrush(Colors.Red);
-		private static SolidColorBrush BlackForeground = new SolidColorBrush(Colors.Black);
-
 		#region Enum
 
 		public enum TransactionTypes
