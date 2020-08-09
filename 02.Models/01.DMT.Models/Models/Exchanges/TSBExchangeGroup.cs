9311cf22b596e0e69b2cd6ba4ed6ca383644057a
chumpon-asaneerat <chumponsenate@gmail.com>
2020-08-09 21:36:43 +0700

    Update 2020-08-09 #34 - Update Exchange/Credit process #56.

---
+++ 02.Models/01.DMT.Models/Models/Exchanges/TSBExchangeGroup.cs
@@ -0,0 +1,31 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Media;
+
+using NLib;
+using NLib.Design;
+using NLib.Reflection;
+
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+using SQLiteNetExtensions.Extensions;
+// required for JsonIgnore attribute.
+using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
+
+#endregion
+
+namespace DMT.Models
+{
+    #region TSBExchangeGroup
+
+    public class TSBExchangeGroup
+    {
+    }
+
+    #endregion
+}
