93baf6aec8dc7594b4db985a7e7e56b62196f179
chumpon-asaneerat <chumponsenate@gmail.com>
2020-08-11 18:50:00 +0700

    Update 2020-08-11 #44 - Update Local/DataCenter/TAxTOD service operation #20.

---
+++ 02.Models/01.DMT.Models/Models/Common/NResult.cs
@@ -0,0 +1,41 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// required for JsonIgnore.
+using Newtonsoft.Json;
+using NLib;
+using NLib.Reflection;
+
+#endregion
+
+namespace DMT.Models
+{
+    public class NError
+    {
+        public bool hasError { get; set; }
+        public int errNum { get; set; }
+        public string errMsg { get; set; }
+    }
+
+    public abstract class NResult
+    {
+        public NError errors { get; set; }
+    }
+
+    public class NResult<T> : NResult
+        where T : class, new()
+    {
+        public T data { get; set; }
+    }
+
+    public class NResult<T, O> : NResult
+        where T : class, new()
+        where O : class, new()
+    {
+        public T data { get; set; }
+        public O Out { get; set; }
+    }
+}
