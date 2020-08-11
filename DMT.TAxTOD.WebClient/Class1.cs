b97ecb9908b6642ddfbee1fc5b8fdb4e8465d4fc
chumpon-asaneerat <chumponsenate@gmail.com>
2020-08-11 16:26:46 +0700

    Update 2020-08-11 #13 - Add DMT.DataCenter.WebClient project. - Add DMT.TAxTOD.WebClient project.

---
+++ DMT.TAxTOD.WebClient/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMT.TAxTOD.WebClient
+{
+    public class Class1
+    {
+    }
+}
