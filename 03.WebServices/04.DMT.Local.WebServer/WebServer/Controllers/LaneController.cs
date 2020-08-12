edce15256c6a167e76b4065e7b87afea40ad63c9
chumpon-asaneerat <chumponsenate@gmail.com>
2020-08-13 04:42:34 +0700

    Update 2020-08-13 #29 - Update Local/DataCenter/TAxTOD service operation #87. - Implements Lane Attendance related process #3. - Implements Lane Payment related process #3.

--- 03.WebServices/04.DMT.Local.WebServer/WebServer/Controllers/LaneController.cs
+++ 03.WebServices/04.DMT.Local.WebServer/WebServer/Controllers/LaneController.cs
@@ -7,6 +7,7 @@ using System.Web.Http;
 using NLib.Reflection;
 
 using DMT.Models;
+using System.Runtime.InteropServices.WindowsRuntime;
 //using DMT.Models.ExtensionMethods;
 
 #endregion
@@ -24,68 +25,141 @@ namespace DMT.Services
         [ActionName(RouteConsts.Lane.CreateAttendance.Name)]
         public NDbResult<LaneAttendance> Create([FromBody] LaneAttendanceCreate value)
         {
-            if (null == value) return null;
-            return LaneAttendance.Create(value.Lane, value.User);
+            NDbResult<LaneAttendance> result;
+            if (null == value)
+            {
+                result = new NDbResult<LaneAttendance>();
+                result.ParameterIsNull();
+                result.data = null;
+            }
+            else
+            {
+                result = LaneAttendance.Create(value.Lane, value.User);
+            }
+            return result;
         }
 
         [HttpPost]
         [ActionName(RouteConsts.Lane.SaveAttendance.Name)]
         public NDbResult<LaneAttendance> SaveAttendance([FromBody] LaneAttendance value)
         {
-            if (null == value) return;
-            Random rand = new Random();
-            if (string.IsNullOrWhiteSpace(value.JobId))
+            NDbResult<LaneAttendance> result;
+            if (null == value)
             {
-                value.JobId = rand.Next(100000).ToString("D5"); // auto generate.
+                result = new NDbResult<LaneAttendance>();
+                result.ParameterIsNull();
+                result.data = null;
             }
-            LaneAttendance.Save(value);
+            else
+            {
+                Random rand = new Random();
+                if (string.IsNullOrWhiteSpace(value.JobId))
+                {
+                    value.JobId = rand.Next(100000).ToString("D5"); // auto generate.
+                }
+                result = LaneAttendance.Save(value);
+            }
+            return result;
         }
 
         [HttpPost]
         [ActionName(RouteConsts.Lane.GetAttendancesByDate.Name)]
         public NDbResult<List<LaneAttendance>> GetAttendancesByDate([FromBody] Search.Lanes.Attendances.ByDate value)
         {
-            if (null == value) return new List<LaneAttendance>();
-            return LaneAttendance.Search(value.Date);
+            NDbResult<List<LaneAttendance>> result;
+            if (null == value)
+            {
+                result = new NDbResult<List<LaneAttendance>>();
+                result.ParameterIsNull();
+                result.data = new List<LaneAttendance>();
+            }
+            else
+            {
+                if (null == value) return new List<LaneAttendance>();
+                result = LaneAttendance.Search(value.Date);
+            }
+            return result;
         }
 
         [HttpPost]
         [ActionName(RouteConsts.Lane.GetAttendancesByUserShift.Name)]
         public NDbResult<List<LaneAttendance>> GetAttendancesByUserShift([FromBody] Search.Lanes.Attendances.ByUserShift value)
         {
-            if (null == value) return new List<LaneAttendance>();
-            return LaneAttendance.Search(value.Shift, value.PlazaGroup, value.RevenueDate);
+            NDbResult<List<LaneAttendance>> result;
+            if (null == value)
+            {
+                result = new NDbResult<List<LaneAttendance>>();
+                result.ParameterIsNull();
+                result.data = new List<LaneAttendance>();
+            }
+            else
+            {
+                result = LaneAttendance.Search(value.Shift, value.PlazaGroup, value.RevenueDate);
+            }
+            return result;
         }
 
         [HttpPost]
         [ActionName(RouteConsts.Lane.GetAllAttendancesByUserShift.Name)]
         public NDbResult<List<LaneAttendance>> GetAllAttendancesByUserShift([FromBody] UserShift value)
         {
-            if (null == value) return new List<LaneAttendance>();
-            return LaneAttendance.Search(value);
+            NDbResult<List<LaneAttendance>> result;
+            if (null == value)
+            {
+                result = new NDbResult<List<LaneAttendance>>();
+                result.ParameterIsNull();
+                result.data = new List<LaneAttendance>();
+            }
+            else
+            {
+                result = LaneAttendance.Search(value);
+            }
+            return result;
         }
 
         [HttpPost]
         [ActionName(RouteConsts.Lane.GetAttendancesByLane.Name)]
         public NDbResult<List<LaneAttendance>> GetAttendancesByLane([FromBody] Search.Lanes.Attendances.ByLane value)
         {
-            if (null == value) return new List<LaneAttendance>();
-            return LaneAttendance.Search(value.Lane);
+            NDbResult<List<LaneAttendance>> result;
+            if (null == value)
+            {
+                result = new NDbResult<List<LaneAttendance>>();
+                result.ParameterIsNull();
+                result.data = new List<LaneAttendance>();
+            }
+            else
+            {
+                result = LaneAttendance.Search(value.Lane);
+            }
+            return result;
         }
 
         [HttpPost]
         [ActionName(RouteConsts.Lane.GetCurrentAttendancesByLane.Name)]
         public NDbResult<LaneAttendance> GetCurrentAttendancesByLane([FromBody] Search.Lanes.Current.AttendanceByLane value)
         {
-            if (null == value) return null;
-            return LaneAttendance.GetCurrentByLane(value.Lane);
+            NDbResult<LaneAttendance> result;
+            if (null == value)
+            {
+                result = new NDbResult<LaneAttendance>();
+                result.ParameterIsNull();
+                result.data = null;
+            }
+            else
+            {
+                result = LaneAttendance.GetCurrentByLane(value.Lane);
+            }
+            return result;
         }
 
         [HttpPost]
         [ActionName(RouteConsts.Lane.GetAllNotHasRevenueEntry.Name)]
         public NDbResult<List<LaneAttendance>> GetAllNotHasRevenueEntry()
         {
-            return LaneAttendance.GetAllNotHasRevenueEntry();
+            NDbResult<List<LaneAttendance>> result;
+            result = LaneAttendance.GetAllNotHasRevenueEntry();
+            return result;
         }
 
         #endregion
@@ -96,54 +170,114 @@ namespace DMT.Services
         [ActionName(RouteConsts.Lane.SavePayment.Name)]
         public NDbResult<LanePayment> Create([FromBody] LanePaymentCreate value)
         {
-            if (null == value) return null;
-            return LanePayment.Create(value.Lane, value.User, 
-                value.Payment, value.Date, value.Amount);
+            NDbResult<LanePayment> result;
+            if (null == value)
+            {
+                result = new NDbResult<LanePayment>();
+                result.ParameterIsNull();
+                result.data = null;
+            }
+            else
+            {
+                result = LanePayment.Create(value.Lane, value.User,
+                    value.Payment, value.Date, value.Amount);
+            }
+            return result;
         }
 
         [HttpPost]
         [ActionName(RouteConsts.Lane.SavePayment.Name)]
         public NDbResult<LanePayment> SavePayment([FromBody] LanePayment value)
         {
-            if (null == value) return;
-            Random rand = new Random();
-            if (string.IsNullOrWhiteSpace(value.ApproveCode))
+            NDbResult<LanePayment> result;
+            if (null == value)
             {
-                value.ApproveCode = rand.Next(10000000).ToString("D8"); // auto generate.
+                result = new NDbResult<LanePayment>();
+                result.ParameterIsNull();
+                result.data = null;
             }
-            LanePayment.Save(value);
+            else
+            {
+                Random rand = new Random();
+                if (string.IsNullOrWhiteSpace(value.ApproveCode))
+                {
+                    value.ApproveCode = rand.Next(10000000).ToString("D8"); // auto generate.
+                }
+                result = LanePayment.Save(value);
+            }
+            return result;
         }
 
         [HttpPost]
         [ActionName(RouteConsts.Lane.GetPaymentsByDate.Name)]
         public NDbResult<List<LanePayment>> GetPaymentsByDate([FromBody] Search.Lanes.Payments.ByDate value)
         {
-            if (null == value) return new List<LanePayment>();
-            return LanePayment.Search(value.Date);
+            NDbResult<List<LanePayment>> result;
+            if (null == value)
+            {
+                result = new NDbResult<List<LanePayment>>();
+                result.ParameterIsNull();
+                result.data = new List<LanePayment>();
+            }
+            else
+            {
+                result = LanePayment.Search(value.Date);
+            }
+            return result;
         }
 
         [HttpPost]
         [ActionName(RouteConsts.Lane.GetPaymentsByUserShift.Name)]
         public NDbResult<List<LanePayment>> GetPaymentsByUserShifts([FromBody] Search.Lanes.Payments.ByUserShift value)
         {
-            if (null == value) return new List<LanePayment>();
-            return LanePayment.Search(value.Shift);
+            NDbResult<List<LanePayment>> result;
+            if (null == value)
+            {
+                result = new NDbResult<List<LanePayment>>();
+                result.ParameterIsNull();
+                result.data = new List<LanePayment>();
+            }
+            else
+            {
+                result = LanePayment.Search(value.Shift);
+            }
+            return result;
         }
 
         [HttpPost]
         [ActionName(RouteConsts.Lane.GetPaymentsByLane.Name)]
         public NDbResult<List<LanePayment>> GetPaymentsByLane([FromBody] Search.Lanes.Payments.ByLane value)
         {
-            if (null == value) return new List<LanePayment>();
-            return LanePayment.Search(value.Lane);
+            NDbResult<List<LanePayment>> result;
+            if (null == value)
+            {
+                result = new NDbResult<List<LanePayment>>();
+                result.ParameterIsNull();
+                result.data = new List<LanePayment>();
+            }
+            else
+            {
+                result = LanePayment.Search(value.Lane);
+            }
+            return result;
         }
 
         [HttpPost]
         [ActionName(RouteConsts.Lane.GetCurrentPaymentsByLane.Name)]
         public NDbResult<LanePayment> GetCurrentPaymentsByLane([FromBody] Search.Lanes.Current.PaymentByLane value)
         {
-            if (null == value) return null;
-            return LanePayment.GetCurrentByLane(value.Lane);
+            NDbResult<LanePayment> result;
+            if (null == value)
+            {
+                result = new NDbResult<LanePayment>();
+                result.ParameterIsNull();
+                result.data = null;
+            }
+            else
+            {
+                result = LanePayment.GetCurrentByLane(value.Lane);
+            }
+            return result;
         }
 
         #endregion
