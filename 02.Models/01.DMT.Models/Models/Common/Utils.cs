b031504743fe9b548efade0101337f24d7b8cc07
chumpon-asaneerat <chumponsenate@gmail.com>
2020-08-15 05:57:17 +0700

    Update 2020-08-15 #7 - Update Local/DataCenter/TAxTOD service operation #175.   - Add Utils.MD5 class.   - Update LocalDbServer.cs     - Add encrypt password in user init method.

--- 02.Models/01.DMT.Models/Models/Common/Utils.cs
+++ 02.Models/01.DMT.Models/Models/Common/Utils.cs
@@ -10,8 +10,42 @@ using System.Threading.Tasks;
 
 namespace DMT.Models
 {
-    public class Utils
+    /// <summary>
+    /// The DMT Utils class.
+    /// </summary>
+    public static class Utils
     {
+        #region MD5
 
+        /// <summary>
+        /// The MD5 Utils.
+        /// </summary>
+        public static class MD5
+        {
+            /// <summary>
+            /// Encrypt original string to MD5 string.
+            /// </summary>
+            /// <param name="value">The original string.</param>
+            /// <returns>Returns encrypt string.</returns>
+            public static string Encrypt(string value)
+            {
+                string ret = string.Empty;
+
+                return ret;
+            }
+            /// <summary>
+            /// Decrypt MD5 string to original.
+            /// </summary>
+            /// <param name="value">The encrypt string.</param>
+            /// <returns>Returns original string.</returns>
+            public static string Decrypt(string value)
+            {
+                string ret = string.Empty;
+
+                return ret;
+            }
+        }
+
+        #endregion
     }
 }
