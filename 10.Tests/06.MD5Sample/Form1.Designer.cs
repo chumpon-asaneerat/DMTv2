8e3c55957c5968f7da60eba88f3d1b844ef22c9b
chumpon-asaneerat <chumponsenate@gmail.com>
2020-08-15 06:33:55 +0700

    Update 2020-08-15 #10 - Update Local/DataCenter/TAxTOD service operation #178.   - Update MD5 sample project #3.

--- 10.Tests/06.MD5Sample/Form1.Designer.cs
+++ 10.Tests/06.MD5Sample/Form1.Designer.cs
@@ -36,12 +36,15 @@
             this.cmdDecrypt = new System.Windows.Forms.Button();
             this.txtOut1 = new System.Windows.Forms.TextBox();
             this.txtOut2 = new System.Windows.Forms.TextBox();
+            this.rbASCII = new System.Windows.Forms.RadioButton();
+            this.rbUTF8v1 = new System.Windows.Forms.RadioButton();
+            this.rbUTF8v2 = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(12, 61);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(57, 17);
             this.label1.TabIndex = 0;
@@ -50,7 +53,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 99);
+            this.label2.Location = new System.Drawing.Point(12, 151);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(37, 17);
             this.label2.TabIndex = 1;
@@ -58,7 +61,7 @@
             // 
             // txtOri
             // 
-            this.txtOri.Location = new System.Drawing.Point(15, 29);
+            this.txtOri.Location = new System.Drawing.Point(15, 81);
             this.txtOri.Name = "txtOri";
             this.txtOri.Size = new System.Drawing.Size(502, 22);
             this.txtOri.TabIndex = 2;
@@ -66,7 +69,7 @@
             // 
             // txtMD5
             // 
-            this.txtMD5.Location = new System.Drawing.Point(15, 119);
+            this.txtMD5.Location = new System.Drawing.Point(15, 171);
             this.txtMD5.Name = "txtMD5";
             this.txtMD5.Size = new System.Drawing.Size(502, 22);
             this.txtMD5.TabIndex = 3;
@@ -74,7 +77,7 @@
             // 
             // cmdEncrypt
             // 
-            this.cmdEncrypt.Location = new System.Drawing.Point(523, 24);
+            this.cmdEncrypt.Location = new System.Drawing.Point(523, 76);
             this.cmdEncrypt.Name = "cmdEncrypt";
             this.cmdEncrypt.Size = new System.Drawing.Size(125, 33);
             this.cmdEncrypt.TabIndex = 4;
@@ -84,7 +87,7 @@
             // 
             // cmdDecrypt
             // 
-            this.cmdDecrypt.Location = new System.Drawing.Point(523, 114);
+            this.cmdDecrypt.Location = new System.Drawing.Point(523, 166);
             this.cmdDecrypt.Name = "cmdDecrypt";
             this.cmdDecrypt.Size = new System.Drawing.Size(125, 33);
             this.cmdDecrypt.TabIndex = 5;
@@ -94,7 +97,7 @@
             // 
             // txtOut1
             // 
-            this.txtOut1.Location = new System.Drawing.Point(15, 57);
+            this.txtOut1.Location = new System.Drawing.Point(15, 109);
             this.txtOut1.Name = "txtOut1";
             this.txtOut1.ReadOnly = true;
             this.txtOut1.Size = new System.Drawing.Size(502, 22);
@@ -102,17 +105,52 @@
             // 
             // txtOut2
             // 
-            this.txtOut2.Location = new System.Drawing.Point(15, 147);
+            this.txtOut2.Location = new System.Drawing.Point(15, 199);
             this.txtOut2.Name = "txtOut2";
             this.txtOut2.ReadOnly = true;
             this.txtOut2.Size = new System.Drawing.Size(502, 22);
             this.txtOut2.TabIndex = 7;
+            // 
+            // rbASCII
+            // 
+            this.rbASCII.AutoSize = true;
+            this.rbASCII.Checked = true;
+            this.rbASCII.Location = new System.Drawing.Point(15, 12);
+            this.rbASCII.Name = "rbASCII";
+            this.rbASCII.Size = new System.Drawing.Size(62, 21);
+            this.rbASCII.TabIndex = 8;
+            this.rbASCII.TabStop = true;
+            this.rbASCII.Text = "ASCII";
+            this.rbASCII.UseVisualStyleBackColor = true;
+            // 
+            // rbUTF8v1
+            // 
+            this.rbUTF8v1.AutoSize = true;
+            this.rbUTF8v1.Location = new System.Drawing.Point(101, 12);
+            this.rbUTF8v1.Name = "rbUTF8v1";
+            this.rbUTF8v1.Size = new System.Drawing.Size(83, 21);
+            this.rbUTF8v1.TabIndex = 9;
+            this.rbUTF8v1.Text = "UTF8 v1";
+            this.rbUTF8v1.UseVisualStyleBackColor = true;
+            // 
+            // rbUTF8v2
+            // 
+            this.rbUTF8v2.AutoSize = true;
+            this.rbUTF8v2.Location = new System.Drawing.Point(208, 12);
+            this.rbUTF8v2.Name = "rbUTF8v2";
+            this.rbUTF8v2.Size = new System.Drawing.Size(83, 21);
+            this.rbUTF8v2.TabIndex = 10;
+            this.rbUTF8v2.Text = "UTF8 v2";
+            this.rbUTF8v2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(673, 281);
+            this.Controls.Add(this.rbUTF8v2);
+            this.Controls.Add(this.rbUTF8v1);
+            this.Controls.Add(this.rbASCII);
             this.Controls.Add(this.txtOut2);
             this.Controls.Add(this.txtOut1);
             this.Controls.Add(this.cmdDecrypt);
@@ -138,6 +176,9 @@
         private System.Windows.Forms.Button cmdDecrypt;
         private System.Windows.Forms.TextBox txtOut1;
         private System.Windows.Forms.TextBox txtOut2;
+        private System.Windows.Forms.RadioButton rbASCII;
+        private System.Windows.Forms.RadioButton rbUTF8v1;
+        private System.Windows.Forms.RadioButton rbUTF8v2;
     }
 }
 
--- 10.Tests/06.MD5Sample/Form1.cs
+++ 10.Tests/06.MD5Sample/Form1.cs
@@ -10,6 +10,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using cypt = System.Security.Cryptography;
+
 #endregion
 
 namespace MD5Sample
@@ -34,6 +36,19 @@ namespace MD5Sample
             {
                 txtOri.Focus();
             }
+
+            if (rbASCII.Checked)
+            {
+                txtOut1.Text = Utils.MD5.ASCII.Encrypt(ori);
+            }
+            else if (rbUTF8v1.Checked)
+            {
+                txtOut1.Text = Utils.MD5.UTF8.Encrypt(ori);
+            }
+            else if (rbUTF8v2.Checked)
+            {
+                txtOut1.Text = Utils.MD5.UTF8v2.Encrypt(ori);
+            }
         }
 
         private void cmdDecrypt_Click(object sender, EventArgs e)
@@ -42,6 +57,19 @@ namespace MD5Sample
             if (string.IsNullOrEmpty(md5))
             {
                 txtMD5.Focus();
+            }
+
+            if (rbASCII.Checked)
+            {
+                txtOut2.Text = Utils.MD5.ASCII.Decrypt(md5);
+            }
+            else if (rbUTF8v1.Checked)
+            {
+                txtOut2.Text = Utils.MD5.UTF8.Decrypt(md5);
+            }
+            else if (rbUTF8v2.Checked)
+            {
+                txtOut2.Text = Utils.MD5.UTF8v2.Decrypt(md5);
             }
         }
 
@@ -57,6 +85,147 @@ namespace MD5Sample
         private void txtMD5_Enter(object sender, EventArgs e)
         {
             txtMD5.SelectAll();
+        }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// The DMT Utils class.
+    /// </summary>
+    public static class Utils
+    {
+        #region MD5
+
+        /// <summary>
+        /// The MD5 Utils class.
+        /// </summary>
+        public static class MD5
+        {
+            #region ASCII
+
+            /// <summary>
+            /// The MD5 ASCII Encoding Utils class.
+            /// </summary>
+            public static class ASCII
+            {
+                /// <summary>
+                /// Encrypt original string to MD5 string.
+                /// </summary>
+                /// <param name="value">The original string.</param>
+                /// <returns>Returns encrypt string.</returns>
+                public static string Encrypt(string value)
+                {
+                    string ret = string.Empty;
+
+                    byte[] asciiBytes = ASCIIEncoding.ASCII.GetBytes(value);
+                    byte[] hashedBytes = cypt.MD5CryptoServiceProvider.Create().ComputeHash(asciiBytes);
+                    ret = BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+
+                    return ret;
+                }
+                /// <summary>
+                /// Decrypt MD5 string to original.
+                /// </summary>
+                /// <param name="value">The encrypt string.</param>
+                /// <returns>Returns original string.</returns>
+                public static string Decrypt(string value)
+                {
+                    string ret = string.Empty;
+
+                    return ret;
+                }
+            }
+
+            #endregion
+
+            #region UTF8
+
+            /// <summary>
+            /// The MD5 UTF8 Encoding Utils class.
+            /// </summary>
+            public static class UTF8
+            {
+                /// <summary>
+                /// Encrypt original string to MD5 string.
+                /// </summary>
+                /// <param name="value">The original string.</param>
+                /// <returns>Returns encrypt string.</returns>
+                public static string Encrypt(string value)
+                {
+                    string ret = string.Empty;
+
+                    // byte array representation of that string
+                    byte[] encodedPassword = new UTF8Encoding().GetBytes(value);
+                    // need MD5 to calculate the hash
+                    cypt.HashAlgorithm algo = (cypt.HashAlgorithm)cypt.CryptoConfig.CreateFromName("MD5");
+                    byte[] hash = algo.ComputeHash(encodedPassword);
+                    // string representation (similar to UNIX format)
+                    ret = BitConverter.ToString(hash)
+                       // without dashes
+                       .Replace("-", string.Empty)
+                       // make lowercase
+                       .ToLower();
+
+                    return ret;
+                }
+                /// <summary>
+                /// Decrypt MD5 string to original.
+                /// </summary>
+                /// <param name="value">The encrypt string.</param>
+                /// <returns>Returns original string.</returns>
+                public static string Decrypt(string value)
+                {
+                    string ret = string.Empty;
+
+                    return ret;
+                }
+            }
+
+            #endregion
+
+            #region UTF8v2
+
+            /// <summary>
+            /// The MD5 UTF8v2 Encoding Utils class.
+            /// </summary>
+            public static class UTF8v2
+            {
+                /// <summary>
+                /// Encrypt original string to MD5 string.
+                /// </summary>
+                /// <param name="value">The original string.</param>
+                /// <returns>Returns encrypt string.</returns>
+                public static string Encrypt(string value)
+                {
+                    string ret = string.Empty;
+
+                    using (var md5 = cypt.MD5.Create())
+                    {
+                        /*
+                        ret = BitConverter.ToString(md5.ComputeHash(Encoding.UTF8.GetBytes(value)))
+                            .Replace("-", string.Empty).ToLower();
+                        */
+                        ret = BitConverter.ToString(md5.ComputeHash(Encoding.Default.GetBytes(value)))
+                            .Replace("-", string.Empty).ToLower();
+                    }
+
+                    return ret;
+                }
+                /// <summary>
+                /// Decrypt MD5 string to original.
+                /// </summary>
+                /// <param name="value">The encrypt string.</param>
+                /// <returns>Returns original string.</returns>
+                public static string Decrypt(string value)
+                {
+                    string ret = string.Empty;
+
+                    return ret;
+                }
+            }
+
+            #endregion
         }
 
         #endregion
