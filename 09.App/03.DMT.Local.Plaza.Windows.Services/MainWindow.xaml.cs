8a3940760b06a56c70f972285dd6d81b91e58f2f
chumpon-asaneerat <chumponsenate@gmail.com>
2020-08-15 22:46:56 +0700

    Update 2020-08-15 #41 - Update Local/DataCenter/TAxTOD service operation #203.   - Replace 03.DMT.Local.Plaza.Windows.Services (Windows Forms -> WPF).

---
+++ 09.App/03.DMT.Local.Plaza.Windows.Services/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+#endregion
+
+namespace DMT
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        private Services.LocalDatabaseWebServer server = null;
+
+        #region Loaded/Unloaded
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (null == server)
+            {
+                server = new Services.LocalDatabaseWebServer();
+                server.Start();
+            }
+        }
+
+        private void Window_Unloaded(object sender, RoutedEventArgs e)
+        {
+            if (null != server)
+            {
+                server.Shutdown();
+            }
+            server = null;
+        }
+
+        #endregion
+    }
+}
