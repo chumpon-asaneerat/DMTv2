d6880e69e6b66636dec48b3b0fa9a74fa5e568df
chumpon-asaneerat <chumponsenate@gmail.com>
2020-08-16 04:55:59 +0700

    Update 2020-08-16 #39 - Update Local/DataCenter/TAxTOD service operation #243.   - User Model update #4.     - Update UserView.sql (fixed problem when no middle name).

--- 02.Models/01.DMT.Models/Views/Scripts/Users/UserView.sql
+++ 02.Models/01.DMT.Models/Views/Scripts/Users/UserView.sql
@@ -1,16 +1,24 @@
 ﻿CREATE VIEW UserView
 AS
 	SELECT [User].*
-		 , TRIM(PRINTF("%s %s %s %s"
-		      , [User].PrefixEN
-			  , [User].FirstNameEN
-			  , [User].MiddleNameEN
-			  , [User].LastNameEN)) AS FullNameEN
-		 , TRIM(PRINTF("%s %s %s %s"
-		      , [User].PrefixTH
-			  , [User].FirstNameTH
-			  , [User].MiddleNameTH
-			  , [User].LastNameTH)) AS FullNameTH
+		 , TRIM(PRINTF("%s %s"
+		          , [User].PrefixEN
+			      , TRIM(PRINTF("%s %s"
+				            , [User].FirstNameEN
+			                , TRIM(PRINTF("%s %s"
+				                   , [User].MiddleNameEN
+			                       , [User].LastNameEN))
+			          ))
+			   )) AS FullNameEN
+		 , TRIM(PRINTF("%s %s"
+		          , [User].PrefixTH
+			      , TRIM(PRINTF("%s %s"
+				            , [User].FirstNameTH
+			                , TRIM(PRINTF("%s %s"
+				                   , [User].MiddleNameTH
+			                       , [User].LastNameTH))
+			          ))
+			   )) AS FullNameTH
 		 , [Role].RoleNameEN, [Role].RoleNameTH, [Role].GroupId
 	  FROM [User]
 	     , [Role]
