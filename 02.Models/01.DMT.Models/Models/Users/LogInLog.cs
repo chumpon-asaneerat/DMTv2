fa55e4d45c176c3c0350df26d9d8b56e8d6a54f8
chumpon-asaneerat <chumponsenate@gmail.com>
2020-08-16 03:47:33 +0700

    Update 2020-08-16 #33 - Update Local/DataCenter/TAxTOD service operation #238.   - LogInLog  Model update #1.     - init new class for keep log in log.

---
+++ 02.Models/01.DMT.Models/Models/Users/LogInLog.cs
@@ -0,0 +1,62 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+using NLib;
+using NLib.Design;
+using NLib.Reflection;
+
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+using SQLiteNetExtensions.Extensions;
+// required for JsonIgnore attribute.
+using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
+using System.Reflection;
+
+#endregion
+
+namespace DMT.Models
+{
+	#region LogInLog
+
+	/// <summary>
+	/// The LogInLog Data Model Class.
+	/// </summary>
+	[TypeConverter(typeof(PropertySorterSupportExpandableTypeConverter))]
+	[Serializable]
+	[JsonObject(MemberSerialization.OptOut)]
+	//[Table("Role")]
+	public class LogInLog : NTable<LogInLog>
+	{
+		#region Internal Variables
+
+		#endregion
+
+		#region Constructor
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public LogInLog() : base() { }
+
+		#endregion
+
+		#region Public Properties
+
+		#endregion
+
+		#region Internal Class
+
+		#endregion
+
+		#region Static Methods
+
+		#endregion
+	}
+
+	#endregion
+}
