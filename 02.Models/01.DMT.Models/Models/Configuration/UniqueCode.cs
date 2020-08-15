db5d31b4e9d8ee7816a74a897fdacc7c78ba7840
chumpon-asaneerat <chumponsenate@gmail.com>
2020-08-16 01:05:35 +0700

    Update 2020-08-16 #5 - Update Local/DataCenter/TAxTOD service operation #212.   - UniqueCode Model update #1.     - init new class for auto gernrate unique code/serial number.

---
+++ 02.Models/01.DMT.Models/Models/Configuration/UniqueCode.cs
@@ -0,0 +1,183 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+using NLib;
+using NLib.Design;
+using NLib.Reflection;
+
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+using SQLiteNetExtensions.Extensions;
+// required for JsonIgnore attribute.
+using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
+
+#endregion
+
+namespace DMT.Models
+{
+	#region UniqueCode
+
+	/// <summary>
+	/// The UniqueCode Data Model Class.
+	/// </summary>
+	[TypeConverter(typeof(PropertySorterSupportExpandableTypeConverter))]
+	[Serializable]
+	[JsonObject(MemberSerialization.OptOut)]
+	//[Table("UniqueCode")]
+	public class UniqueCode : NTable<UniqueCode>
+	{
+		#region Enum
+
+		public enum ResetMode
+		{
+			Yearly = 1,
+			Monthly = 2
+		}
+
+		#endregion
+
+		#region Intenral Variables
+
+		private string _Key = string.Empty;
+		private ResetMode _Mode = ResetMode.Yearly;
+		private string _Prefix = string.Empty;
+		private int _LastNumber = 0;
+		private DateTime _LastUpdate = DateTime.MinValue;
+
+		#endregion
+
+		#region Constructor
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public UniqueCode() : base() { }
+
+		#endregion
+
+		#region Public Properties
+
+		/// <summary>
+		/// Gets or sets Key
+		/// </summary>
+		[Category("Common")]
+		[Description("Gets or sets Key")]
+		[PrimaryKey, MaxLength(50)]
+		[PeropertyMapName("Key")]
+		public string Key
+		{
+			get
+			{
+				return _Key;
+			}
+			set
+			{
+				if (_Key != value)
+				{
+					_Key = value;
+					this.RaiseChanged("Key");
+				}
+			}
+		}
+		/// <summary>
+		/// Gets or sets Mode
+		/// </summary>
+		[Category("Common")]
+		[Description("Gets or sets Mode")]
+		[PeropertyMapName("Mode")]
+		public ResetMode Mode
+		{
+			get
+			{
+				return _Mode;
+			}
+			set
+			{
+				if (_Mode != value)
+				{
+					_Mode = value;
+					this.RaiseChanged("Mode");
+				}
+			}
+		}
+		/// <summary>
+		/// Gets or sets Prefix
+		/// </summary>
+		[Category("Common")]
+		[Description("Gets or sets Prefix")]
+		[PrimaryKey, MaxLength(30)]
+		[PeropertyMapName("Prefix")]
+		public string Prefix
+		{
+			get
+			{
+				return _Prefix;
+			}
+			set
+			{
+				if (_Prefix != value)
+				{
+					_Prefix = value;
+					this.RaiseChanged("Prefix");
+				}
+			}
+		}
+		/// <summary>
+		/// Gets or sets Last Number
+		/// </summary>
+		[Category("Common")]
+		[Description("Gets or sets Last Number")]
+		[PeropertyMapName("LastNumber")]
+		public int LastNumber
+		{
+			get
+			{
+				return _LastNumber;
+			}
+			set
+			{
+				if (_LastNumber != value)
+				{
+					_LastNumber = value;
+					this.RaiseChanged("LastNumber");
+				}
+			}
+		}
+		/// <summary>
+		/// Gets or sets Last Update
+		/// </summary>
+		[Category("Common")]
+		[Description("Gets or sets Last Update")]
+		[ReadOnly(true)]
+		[PeropertyMapName("LastUpdate")]
+		public DateTime LastUpdate
+		{
+			get
+			{
+				return _LastUpdate;
+			}
+			set
+			{
+				if (_LastUpdate != value)
+				{
+					_LastUpdate = value;
+					this.RaiseChanged("LastUpdate");
+				}
+			}
+		}
+
+		#endregion
+
+		#region Static Methods
+
+
+		#endregion
+	}
+
+	#endregion
+}
