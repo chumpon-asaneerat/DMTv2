fb19d921dedf6d9afd33542abe8e9e2459579540
chumpon-asaneerat <chumponsenate@gmail.com>
2020-08-16 03:53:42 +0700

    Update 2020-08-16 #34 - Update Local/DataCenter/TAxTOD service operation #238.   - LogInLog  Model update #2.

--- 02.Models/01.DMT.Models/Models/Users/LogInLog.cs
+++ 02.Models/01.DMT.Models/Models/Users/LogInLog.cs
@@ -32,7 +32,46 @@ namespace DMT.Models
 	//[Table("Role")]
 	public class LogInLog : NTable<LogInLog>
 	{
+		#region Enum
+
+		/// <summary>
+		/// The User Coupon Transaction Types
+		/// </summary>
+		public enum TransactionTypes
+		{
+			/// <summary>
+			/// Success
+			/// </summary>
+			Success = 0,
+			/// <summary>
+			/// Invalid Password
+			/// </summary>
+			InvalidPassword = 1,
+			/// <summary>
+			/// Password Expired
+			/// </summary>
+			PasswordExpired = 2,
+			/// <summary>
+			/// Cancel (Reserved).
+			/// </summary>
+			Cancel = 9
+		}
+
+		#endregion
+
 		#region Internal Variables
+
+		private int _TransactionId = 0;
+		private DateTime _TransactionDate = DateTime.MinValue;
+		private TransactionTypes _TransactionType = TransactionTypes.Success;
+
+		private string _TSBId = string.Empty;
+		private string _TSBNameEN = string.Empty;
+		private string _TSBNameTH = string.Empty;
+
+		private string _UserId = string.Empty;
+		private string _FullNameEN = string.Empty;
+		private string _FullNameTH = string.Empty;
 
 		#endregion
 
@@ -47,9 +86,336 @@ namespace DMT.Models
 
 		#region Public Properties
 
+		#region Common
+
+		/// <summary>
+		/// Gets or sets TransactionId
+		/// </summary>
+		[Category("Common")]
+		[Description(" Gets or sets TransactionId")]
+		[ReadOnly(true)]
+		[PrimaryKey, AutoIncrement]
+		[PeropertyMapName("TransactionId")]
+		public int TransactionId
+		{
+			get
+			{
+				return _TransactionId;
+			}
+			set
+			{
+				if (_TransactionId != value)
+				{
+					_TransactionId = value;
+					this.RaiseChanged("TransactionId");
+				}
+			}
+		}
+		/// <summary>
+		/// Gets or sets Transaction Date.
+		/// </summary>
+		[Category("Common")]
+		[Description(" Gets or sets Transaction Date")]
+		[ReadOnly(true)]
+		[PeropertyMapName("TransactionDate")]
+		public DateTime TransactionDate
+		{
+			get
+			{
+				return _TransactionDate;
+			}
+			set
+			{
+				if (_TransactionDate != value)
+				{
+					_TransactionDate = value;
+					this.RaiseChanged("TransactionDate");
+					this.RaiseChanged("TransactionDateString");
+					this.RaiseChanged("TransactionTimeString");
+					this.RaiseChanged("TransactionDateTimeString");
+				}
+			}
+		}
+		/// <summary>
+		/// Gets Transaction Date String.
+		/// </summary>
+		[Category("Common")]
+		[Description("Gets Transaction Date String.")]
+		[ReadOnly(true)]
+		[JsonIgnore]
+		[Ignore]
+		public string TransactionDateString
+		{
+			get
+			{
+				var ret = (this.TransactionDate == DateTime.MinValue) ? "" : this.TransactionDate.ToThaiDateTimeString("dd/MM/yyyy");
+				return ret;
+			}
+			set { }
+		}
+		/// <summary>
+		/// Gets Transaction Time String.
+		/// </summary>
+		[Category("Common")]
+		[Description("Gets Transaction Time String.")]
+		[ReadOnly(true)]
+		[JsonIgnore]
+		[Ignore]
+		public string TransactionTimeString
+		{
+			get
+			{
+				var ret = (this.TransactionDate == DateTime.MinValue) ? "" : this.TransactionDate.ToThaiTimeString();
+				return ret;
+			}
+			set { }
+		}
+		/// <summary>
+		/// Gets Transaction Date Time String.
+		/// </summary>
+		[Category("Common")]
+		[Description("Gets Transaction Date Time String.")]
+		[ReadOnly(true)]
+		[JsonIgnore]
+		[Ignore]
+		public string TransactionDateTimeString
+		{
+			get
+			{
+				var ret = (this.TransactionDate == DateTime.MinValue) ? "" : this.TransactionDate.ToThaiDateTimeString("dd/MM/yyyy HH:mm:ss");
+				return ret;
+			}
+			set { }
+		}
+		/// <summary>
+		/// Gets or sets Transaction Type.
+		/// </summary>
+		[Category("Common")]
+		[Description("Gets or sets Transaction Type.")]
+		[ReadOnly(true)]
+		[PeropertyMapName("TransactionType")]
+		public TransactionTypes TransactionType
+		{
+			get { return _TransactionType; }
+			set
+			{
+				if (_TransactionType != value)
+				{
+					_TransactionType = value;
+					this.RaiseChanged("TransactionType");
+				}
+			}
+		}
+
+		#endregion
+
+		#region TSB
+
+		/// <summary>
+		/// Gets or sets TSBId.
+		/// </summary>
+		[Category("TSB")]
+		[Description("Gets or sets TSBId.")]
+		[ReadOnly(true)]
+		[MaxLength(10)]
+		[PeropertyMapName("TSBId")]
+		public string TSBId
+		{
+			get
+			{
+				return _TSBId;
+			}
+			set
+			{
+				if (_TSBId != value)
+				{
+					_TSBId = value;
+					this.RaiseChanged("TSBId");
+				}
+			}
+		}
+		/// <summary>
+		/// Gets or sets TSB Name EN.
+		/// </summary>
+		[Category("TSB")]
+		[Description("Gets or sets TSB Name EN.")]
+		[ReadOnly(true)]
+		[Ignore]
+		[PeropertyMapName("TSBNameEN")]
+		public virtual string TSBNameEN
+		{
+			get
+			{
+				return _TSBNameEN;
+			}
+			set
+			{
+				if (_TSBNameEN != value)
+				{
+					_TSBNameEN = value;
+					this.RaiseChanged("TSBNameEN");
+				}
+			}
+		}
+		/// <summary>
+		/// Gets or sets TSB Name TH.
+		/// </summary>
+		[Category("TSB")]
+		[Description("Gets or sets TSB Name TH.")]
+		[ReadOnly(true)]
+		[Ignore]
+		[PeropertyMapName("TSBNameTH")]
+		public virtual string TSBNameTH
+		{
+			get
+			{
+				return _TSBNameTH;
+			}
+			set
+			{
+				if (_TSBNameTH != value)
+				{
+					_TSBNameTH = value;
+					this.RaiseChanged("TSBNameTH");
+				}
+			}
+		}
+
+		#endregion
+
+		#region User
+
+		/// <summary>
+		/// Gets or sets User Id
+		/// </summary>
+		[Category("User")]
+		[Description("Gets or sets User Id.")]
+		[ReadOnly(true)]
+		[MaxLength(10)]
+		[PeropertyMapName("UserId")]
+		public string UserId
+		{
+			get
+			{
+				return _UserId;
+			}
+			set
+			{
+				if (_UserId != value)
+				{
+					_UserId = value;
+					this.RaiseChanged("UserId");
+				}
+			}
+		}
+		/// <summary>
+		/// Gets or sets User Full Name EN
+		/// </summary>
+		[Category("User")]
+		[Description("Gets or sets User Full Name EN.")]
+		[ReadOnly(true)]
+		[Ignore]
+		[PeropertyMapName("FullNameEN")]
+		public virtual string FullNameEN
+		{
+			get
+			{
+				return _FullNameEN;
+			}
+			set
+			{
+				if (_FullNameEN != value)
+				{
+					_FullNameEN = value;
+					this.RaiseChanged("FullNameEN");
+				}
+			}
+		}
+		/// <summary>
+		/// Gets or sets User Full Name TH
+		/// </summary>
+		[Category("User")]
+		[Description("Gets or sets User Full Name TH.")]
+		[ReadOnly(true)]
+		[Ignore]
+		[PeropertyMapName("FullNameTH")]
+		public virtual string FullNameTH
+		{
+			get
+			{
+				return _FullNameTH;
+			}
+			set
+			{
+				if (_FullNameTH != value)
+				{
+					_FullNameTH = value;
+					this.RaiseChanged("FullNameTH");
+				}
+			}
+		}
+
+		#endregion
+
 		#endregion
 
 		#region Internal Class
+
+		/// <summary>
+		/// The internal FKs class for query data.
+		/// </summary>
+		public class FKs : LogInLog, IFKs<LogInLog>
+		{
+			#region TSB
+
+			/// <summary>
+			/// Gets or sets TSB Name EN.
+			/// </summary>
+			[MaxLength(100)]
+			[PeropertyMapName("TSBNameEN")]
+			public override string TSBNameEN
+			{
+				get { return base.TSBNameEN; }
+				set { base.TSBNameEN = value; }
+			}
+			/// <summary>
+			/// Gets or sets TSB Name TH.
+			/// </summary>
+			[MaxLength(100)]
+			[PeropertyMapName("TSBNameTH")]
+			public override string TSBNameTH
+			{
+				get { return base.TSBNameTH; }
+				set { base.TSBNameTH = value; }
+			}
+
+			#endregion
+
+			#region User
+
+			/// <summary>
+			/// Gets or sets Full Name EN.
+			/// </summary>
+			[MaxLength(100)]
+			[PeropertyMapName("FullNameEN")]
+			public override string FullNameEN
+			{
+				get { return base.FullNameEN; }
+				set { base.FullNameEN = value; }
+			}
+			/// <summary>
+			/// Gets or sets Full Name TH.
+			/// </summary>
+			[MaxLength(100)]
+			[PeropertyMapName("FullNameTH")]
+			public override string FullNameTH
+			{
+				get { return base.FullNameTH; }
+				set { base.FullNameTH = value; }
+			}
+
+			#endregion
+		}
 
 		#endregion
 
