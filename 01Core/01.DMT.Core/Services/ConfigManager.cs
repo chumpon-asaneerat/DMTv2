18abb75ee5fd9d1d0f9264c436a60ef85c19385a
chumpon-asaneerat <chumponsenate@gmail.com>
2020-08-17 00:16:14 +0700

    Update 2020-08-17 #1 - Update Local/DataCenter/TAxTOD service operation #260.   - Implements Config Manager #2.

--- 01Core/01.DMT.Core/Services/ConfigManager.cs
+++ 01Core/01.DMT.Core/Services/ConfigManager.cs
@@ -12,6 +12,93 @@ using Newtonsoft.Json;
 
 namespace DMT.Services
 {
+    #region Plaza Config and related classes
+
+    public class WebServiceConfig
+    {
+        public WebServiceConfig()
+        {
+            this.Protocol = "http";
+            this.HostName = "localhost";
+            this.PortNumber = 9000;
+        }
+
+        public string Protocol { get; set; }
+        public string HostName { get; set; }
+        public int PortNumber { get; set; }
+    }
+
+    public class LocalWebServiceConfig
+    {
+        public LocalWebServiceConfig() 
+        {
+            this.Http = new WebServiceConfig()
+            {
+                Protocol = "http",
+                HostName = "localhost",
+                PortNumber = 9000
+            };
+
+            this.WebSocket = new WebServiceConfig()
+            {
+                Protocol = "ws",
+                HostName = "localhost",
+                PortNumber = 9100
+            };
+        }
+
+        public WebServiceConfig Http { get; set; }
+        public WebServiceConfig WebSocket { get; set; }
+    }
+
+    public class TAxTODWebServiceConfig
+    {
+        public TAxTODWebServiceConfig()
+        {
+            this.Http = new WebServiceConfig()
+            {
+                Protocol = "http",
+                HostName = "localhost",
+                PortNumber = 3000
+            };
+        }
+
+        public WebServiceConfig Http { get; set; }
+    }
+
+    public class DCWebServiceConfig
+    {
+        public DCWebServiceConfig()
+        {
+            this.Http = new WebServiceConfig()
+            {
+                Protocol = "http",
+                HostName = "localhost",
+                PortNumber = 3000
+            };
+        }
+
+        public WebServiceConfig Http { get; set; }
+    }
+
+    public class PlazaConfig
+    {
+        public PlazaConfig() : base()
+        {
+            this.Local = new LocalWebServiceConfig();
+            this.TAxTOD = new TAxTODWebServiceConfig();
+            this.DC = new DCWebServiceConfig();
+        }
+
+        public LocalWebServiceConfig Local { get; set; }
+        public TAxTODWebServiceConfig TAxTOD { get; set; }
+        public DCWebServiceConfig DC { get; set; }
+    }
+
+    #endregion
+
+    #region ConfigManager
+
     /// <summary>
     /// The ConfigManager class.
     /// </summary>
@@ -45,6 +132,7 @@ namespace DMT.Services
 
         private Thread _th = null;
         private string _fileName = NJson.LocalConfigFile("plaza.config.json");
+        private PlazaConfig _plazaCfg = new PlazaConfig();
 
         #endregion
 
@@ -88,11 +176,25 @@ namespace DMT.Services
 
         private void UpdateConfig()
         {
-            if (!NJson.ConfigExists(_fileName))
+            lock (this)
             {
-                
+                if (!NJson.ConfigExists(_fileName))
+                {
+                    if (null == _plazaCfg)
+                    {
+                        _plazaCfg = new PlazaConfig();
+                    }
+                    NJson.SaveToFile(_plazaCfg, _fileName);
+                }
+
+                _plazaCfg = NJson.LoadFromFile<PlazaConfig>(_fileName);
+                if (null == _plazaCfg)
+                {
+                    // Read file failed.
+                    _plazaCfg = new PlazaConfig();
+                    NJson.SaveToFile(_plazaCfg, _fileName);
+                }
             }
-            //_plaza = NJson.LoadFromFile<Plaza>(_fileName);
         }
 
         #endregion
@@ -136,12 +238,17 @@ namespace DMT.Services
         /// Gets is service is running.
         /// </summary>
         public bool IsRunning { get; private set; }
-        /*
         /// <summary>
         /// Gets current plaza app information.
         /// </summary>
-        public Plaza Plaza { get { return _plaza; } }
-        */
+        public PlazaConfig Plaza 
+        { 
+            get { return _plazaCfg; }
+            set { }
+        }
+
         #endregion
     }
+
+    #endregion
 }
