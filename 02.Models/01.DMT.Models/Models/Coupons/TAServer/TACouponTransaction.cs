f24cd8046652223423aec926751a04252e8c036f
chumpon-asaneerat <chumponsenate@gmail.com>
2020-08-17 07:29:58 +0700

    Update 2020-08-17 #12 - Update Local/DataCenter/TAxTOD service operation #269.   - TAxTOD.WebClient Plaza oprations - Coupons     - Add get list api.

---
+++ 02.Models/01.DMT.Models/Models/Coupons/TAServer/TACouponTransaction.cs
@@ -0,0 +1,30 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace DMT.Models
+{
+    public class TAServerCouponTransaction
+    {
+        public int CouponPK { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public string TSBId { get; set; }
+        public int CouponType { get; set; }
+        public string SerialNo { get; set; }
+        public decimal Price { get; set; }
+        public string UserId { get; set; }
+        public DateTime UserReceiveDate { get; set; }
+        public int CouponStatus { get; set; }
+        public DateTime SoldDate { get; set; }
+        public string SoldBy { get; set; }
+        public string LaneId { get; set; }
+        public int FinishFlag { get; set; }
+        public DateTime SapChooseDate { get; set; }
+    }
+}
---
+++ 03.WebServices/03.DMT.TAxTOD.WebClient/Services/Operations/PlazaOperations.Coupon.cs
@@ -0,0 +1,101 @@
+﻿#region Usings
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+using RestSharp;
+
+using DMT.Models;
+
+#endregion
+
+namespace DMT.Services
+{
+    partial class TAxTODOperations
+    {
+        #region Internal Variables
+
+        private CouponOperations _Coupon_Ops = null;
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Gets Coupons Operations.
+        /// </summary>
+        public CouponOperations Coupons
+        {
+            get
+            {
+                if (null == _Coupon_Ops)
+                {
+                    lock (this)
+                    {
+                        _Coupon_Ops = new CouponOperations();
+                    }
+                }
+                return _Coupon_Ops;
+            }
+        }
+
+        #endregion
+
+        #region CouponOperations
+
+        public class CouponOperations
+        {
+            #region Constructor
+
+            /// <summary>
+            /// Constructor.
+            /// </summary>
+            internal CouponOperations() : base() { }
+
+            #endregion
+
+            #region Public Methods
+
+            #region TA Server Coupon Transaction
+
+            public NRestResult<List<TAServerCouponTransaction>> GetTAServerCouponTransactions(string tsbId, string userId, int? transactionType, int? couponType)
+            {
+                NRestClient.WebProtocol protocol =
+                    (ConfigManager.Instance.Plaza.TAxTOD.Http.Protocol == "http") ?
+                    NRestClient.WebProtocol.http : NRestClient.WebProtocol.https;
+                string hostName = ConfigManager.Instance.Plaza.TAxTOD.Http.HostName;
+                int portNo = ConfigManager.Instance.Plaza.TAxTOD.Http.PortNumber;
+
+                NRestResult<List<TAServerCouponTransaction>> ret;
+
+                var url = "api/users/coupons/getlist";
+                var value = new
+                {
+                    tsbid = tsbId,
+                    userid = userId,
+                    transactiontype = transactionType,
+                    coupontype = couponType
+                };
+                if (null != value)
+                {
+                    ret = NRestClient.Create(protocol: protocol, host: hostName, port: portNo)
+                        .Execute<List<TAServerCouponTransaction>>(url, value);
+                }
+                else
+                {
+                    ret = new NRestResult<List<TAServerCouponTransaction>>();
+                    ret.ParameterIsNull();
+                }
+                return ret;
+            }
+
+            #endregion
+
+            #endregion
+        }
+
+        #endregion
+    }
+}
