b02e2bf201dda595bb33cb54f237b65c74b9e22f
chumpon-asaneerat <chumponsenate@gmail.com>
2020-08-23 14:59:32 +0700

    Update 2020-08-23 #2 - Update Local/DataCenter/TAxTOD service operation #295.   - Update TA Server Coupon model #1.

--- 02.Models/01.DMT.Models/Models/Coupons/TAServer/TACouponTransaction.cs
+++ 02.Models/01.DMT.Models/Models/Coupons/TAServer/TACouponTransaction.cs
@@ -13,18 +13,26 @@ namespace DMT.Models
     public class TAServerCouponTransaction
     {
         public int? CouponPK { get; set; }
+
+        public int? TransactionId { get; set; } // save only.
+
         public DateTime? TransactionDate { get; set; }
+
         public string TSBId { get; set; }
+
+        public DateTime? SapChooseDate { get; set; }
         public int? CouponType { get; set; }
         public string SerialNo { get; set; }
         public decimal? Price { get; set; }
+        public int? CouponStatus { get; set; }
+
+        public string LaneId { get; set; }
         public string UserId { get; set; }
         public DateTime? UserReceiveDate { get; set; }
-        public int? CouponStatus { get; set; }
+
         public DateTime? SoldDate { get; set; }
         public string SoldBy { get; set; }
-        public string LaneId { get; set; }
+
         public int? FinishFlag { get; set; }
-        public DateTime? SapChooseDate { get; set; }
     }
 }
