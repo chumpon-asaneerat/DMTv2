385e72c4be7c227cc134427c9b2b53880aca4179
chumpon-asaneerat <chumponsenate@gmail.com>
2020-08-30 00:01:08 +0700

    Update 2020-08-30 #1 - Update Local/DataCenter/TAxTOD service operation #341.   - Update owin server project #5.   - Update owin client project #5.

--- 10.Tests/09.OwinClientSample/Form1.cs
+++ 10.Tests/09.OwinClientSample/Form1.cs
@@ -10,6 +10,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Newtonsoft;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using RestSharp;
+using RestSharp.Authenticators;
+using RestSharp.Serializers.NewtonsoftJson;
+
 #endregion
 
 namespace OwinClientSample
@@ -29,16 +36,11 @@ namespace OwinClientSample
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            pgReq.SelectedObject = new { Name = "" };
+            pgReq.SelectedObject = new { Name = "Test" };
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-            var req = pgReq.SelectedObject;
-            pgRes.SelectedObject = null;
-            // send
-
-            pgRes.SelectedObject = null;
         }
 
         #endregion
@@ -47,7 +49,34 @@ namespace OwinClientSample
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            string BaseUrl = @"http://localhost:8000";
+            string actionUrl = @"api/test/getsample";
+            var req = pgReq.SelectedObject;
+            pgRes.SelectedObject = null;
+            // send
+            var client = new RestClient(BaseUrl);
+            //client.ReadWriteTimeout = timeout;
+            client.Timeout = 1000;
+            /*
+            if (!string.IsNullOrWhiteSpace(username) && !string.IsNullOrWhiteSpace(password))
+            {
+                client.Authenticator = new HttpBasicAuthenticator(username, password);
+            }
+            */
+            //client.CachePolicy = new HttpRequestCachePolicy(HttpRequestCacheLevel.BypassCache);
+            client.UseNewtonsoftJson();
+            var request = new RestRequest(actionUrl, Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            if (null != req)
+            {
+                request.AddJsonBody(req);
+            }
+
+            var response = client.Execute(request);
+            if (null != response)
+            {
+                pgRes.SelectedObject = JsonConvert.DeserializeObject(response.Content);
+            }
         }
 
         #endregion
