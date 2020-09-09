1f2cc771f9ab77d2e2651e48c9dbf6fa3ce9b7cf
chumpon-asaneerat <chumponsenate@gmail.com>
2020-09-09 14:48:16 +0700

    Update 2020-09-09 #16 - Update Local/DataCenter/TAxTOD service operation #437.   - TOD Implements Revenue Entry (sync process) #97.

---
+++ 05.Controls/01.DMT.Controls/TOD/Controls/Revenue/View/SelectLaneView.xaml.cs
@@ -0,0 +1,48 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+#endregion
+
+namespace DMT.TOD.Controls.Revenue.View
+{
+    /// <summary>
+    /// Interaction logic for SelectLaneView.xaml
+    /// </summary>
+    public partial class SelectLaneView : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public SelectLaneView()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Loaded/Unloaded
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        public void Setup(List<Models.LaneAttendance> lanes)
+        {
+            listView.ItemsSource = lanes;
+        }
+    }
+}
