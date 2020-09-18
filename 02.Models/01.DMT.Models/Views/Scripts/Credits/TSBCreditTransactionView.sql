bc4ae2c7a6e8c8ee0375d3fdba43e61b604741a1
chumpon-asaneerat <chumponsenate@gmail.com>
2020-09-18 23:21:39 +0700

    Update 2020-09-18 #3 - Update TA Credit Exchange #21.   - TA Implements Internal Exchange #21.

--- 02.Models/01.DMT.Models/Views/Scripts/Credits/TSBCreditTransactionView.sql
+++ 02.Models/01.DMT.Models/Views/Scripts/Credits/TSBCreditTransactionView.sql
@@ -1,6 +1,16 @@
 ﻿CREATE VIEW TSBCreditTransactionView
 AS
+	/*
 	SELECT TSBCreditTransaction.*
 	     , TSB.TSBNameEN, TSB.TSBNameTH
 	  FROM TSBCreditTransaction, TSB
+	 WHERE TSBCreditTransaction.TSBId = TSB.TSBId
+	 */
+	SELECT TSBCreditTransaction.*
+	     , TSB.TSBNameEN, TSB.TSBNameTH
+		 , IFNULL(sup.FullNameEN, '') AS SupervisorNameEN
+		 , IFNULL(sup.FullNameTH, '') AS SupervisorNameTH
+	  FROM TSBCreditTransaction
+	     , TSB
+	       LEFT JOIN [UserView] sup ON (TSBCreditTransaction.SupervisorId = sup.UserId) 
 	 WHERE TSBCreditTransaction.TSBId = TSB.TSBId
