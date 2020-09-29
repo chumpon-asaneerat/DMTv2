aa1b69452f9ba102104690d48eebe6292a7a4056
chumpon-asaneerat <chumponsenate@gmail.com>
2020-09-30 06:25:52 +0700

    Ported DMTv2 to DMT.TAxTODv1 #2 - Ported DMT.Core project checked. -  DMT.Core project defind conditional compilation symbols: USE_PROGRAM_DATA; - Ported DMT.Smartcard project checked.

--- 01Core/01.DMT.Core/Services/Cmd.cs
+++ 01Core/01.DMT.Core/Services/Cmd.cs
@@ -13,26 +13,44 @@ namespace DMT.Services
 
         public bool UseShellExecute { get; set; }
         public bool RedirectStandardOutput { get; set; }
+        public bool RedirectStandardError { get; set; }
         public bool CreateNoWindow { get; set; }
 
         public CommandLine()
         {
             FileName = "netsh.exe";
             UseShellExecute = false;
-            RedirectStandardOutput = true;
+            RedirectStandardOutput = false;
+            RedirectStandardError = false;
             CreateNoWindow = true;
-
         }
 
         public void Run(string arguments)
         {
-            Process process = new Process();
-            process.StartInfo.FileName = FileName;
-            process.StartInfo.Arguments = arguments;
-            process.StartInfo.UseShellExecute = UseShellExecute;
-            process.StartInfo.RedirectStandardOutput = RedirectStandardOutput;
-            process.StartInfo.CreateNoWindow = CreateNoWindow;
-            process.Start();
+            var psi = new ProcessStartInfo();
+            psi.FileName = FileName;
+            psi.Arguments = arguments;
+            psi.UseShellExecute = UseShellExecute;
+            psi.RedirectStandardOutput = RedirectStandardOutput;
+            psi.RedirectStandardError = RedirectStandardError;
+            psi.CreateNoWindow = CreateNoWindow;
+
+            using (var process = Process.Start(psi))
+            {
+                if (RedirectStandardOutput)
+                {
+                    process.OutputDataReceived += (sender, eventArgs) => Console.WriteLine("OUTPUT: " + eventArgs.Data);
+                    process.BeginOutputReadLine();
+                }
+
+                if (RedirectStandardError)
+                {
+                    process.ErrorDataReceived += (sender, eventArgs) => Console.WriteLine("ERROR: " + eventArgs.Data);
+                    process.BeginErrorReadLine();
+                }
+
+                process.WaitForExit();
+            }
         }
     }
 }
