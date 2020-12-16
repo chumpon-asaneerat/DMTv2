5b450fc054047355f19f658004562a8bdef6373c
chumpon-asaneerat <chumponsenate@gmail.com>
2020-12-16 09:48:35 +0700

    Ported DMTv2 to DMT.TAxTODv1 #265 - DMT.TAxTODv1 Rework #35   - DMT.Models project.     - Rework User.cs       - Add PasswordDateString, PasswordTimeString, PasswordDateTimeString property.     - Rework TSBShift.cs.     - Rework TSBShiftView.sql.     - Update LocalDbServer.cs       - add init TSBShiftView.

--- 02.Models/01.DMT.Models/Views/Scripts/Shifts/TSBShiftView.sql
+++ 02.Models/01.DMT.Models/Views/Scripts/Shifts/TSBShiftView.sql
@@ -3,11 +3,8 @@ AS
 	SELECT TSBShift.*
 		 , TSB.TSBNameEN, TSB.TSBNameTH
 		 , [Shift].ShiftNameEN, [Shift].ShiftNameTH
-		 --, UserView.FullNameEN, UserView.FullNameTH
 	  FROM TSBShift
 		 , TSB
 	     , [Shift]
-		 --, UserView
 	 WHERE TSBShift.TSBId = TSB.TSBId
 	   AND TSBShift.ShiftId = [Shift].ShiftId
-	   --AND TSBShift.UserId = UserView.UserId
